cad1a3b54ad48f2d97153c1d1d226c3f917697e1
Loris Degioanni <loris@degio.org>
2013-09-11 11:48:55 -0700

    node webstress script

---
+++ stress/webstress/nodewebstress.js
@@ -0,0 +1,48 @@
+var MAX_VIDEO_REQUESTS = 1000;
+
+var http = require('http');
+var j = 0;
+var cnt = 0;
+
+var options = {
+  host: '127.0.0.1',
+  path: '/search.php',
+  port: 80,
+  agent:false
+};
+
+callback = function(response) {
+
+  var str;
+
+  //another chunk of data has been recieved, so append it to `str`
+  response.on('data', function (chunk) {
+    str += chunk;
+  });
+
+  //the whole response has been recieved, so we just print it out here
+  response.on('end', function () {
+//    console.log(str);
+  });
+
+  cnt++;
+  if(cnt % 100 == 0)
+  {
+    console.log(cnt.toString());  
+  }
+}
+
+function req_loop()
+{
+  j++;
+  options.path = '/' + j.toString();
+  http.request(options, callback).end();
+  if(j < MAX_VIDEO_REQUESTS)
+  {
+    setTimeout(req_loop, 0);
+  }
+}
+
+http.globalAgent.maxSockets = 400;
+req_loop()
+
