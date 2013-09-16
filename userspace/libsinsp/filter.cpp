26aef75f0bef990a26b0e0c97243a92ab7029bc3
Loris Degioanni <loris@draios.com>
2013-09-15 22:53:46 -0700

    weekend project: filtering language for libsinsp. Still many features missing (need another weekend), but the core stuff is there.

---
+++ userspace/libsinsp/filter.cpp
@@ -0,0 +1,379 @@
+//
+// Why isn't this parser written using antlr or some other parser generator?
+// Essentially, after dealing with the pain that stuff in the past, and fighting for a day
+// to configure everything with crappy documentation and code that doesn't compile,
+// I decided that I agree with this http://mortoray.com/2012/07/20/why-i-dont-use-a-parser-generator/
+// and thocreate with a manually written parser. The grammar is simple enough that it's not
+// going to take more time. On the other hand I will avoid a crappy dependency that breaks my 
+//code at every new release, and I will have a cleaner and easier to understand code base.
+//
+
+#include "sinsp.h"
+#include "sinsp_int.h"
+#include "filter.h"
+
+///////////////////////////////////////////////////////////////////////////////
+// sinsp_filter_check implementation
+///////////////////////////////////////////////////////////////////////////////
+sinsp_filter_check::sinsp_filter_check()
+{
+	m_boolop = BO_NONE;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// sinsp_filter_check_comm implementation
+///////////////////////////////////////////////////////////////////////////////
+bool sinsp_filter_check_comm::recognize_operand(string operand)
+{
+	if(operand == "comm")
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void sinsp_filter_check_comm::parse_operand2(string val)
+{
+	m_comm = val;
+}
+
+bool sinsp_filter_check_comm::run(sinsp_evt *evt)
+{
+	ASSERT(evt);
+
+	sinsp_threadinfo* tinfo = evt->get_thread_info();
+
+	if(tinfo != NULL && tinfo->get_comm() == m_comm)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// sinsp_filter_check_tid implementation
+///////////////////////////////////////////////////////////////////////////////
+bool sinsp_filter_check_tid::recognize_operand(string operand)
+{
+	if(operand == "tid")
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+void sinsp_filter_check_tid::parse_operand2(string val)
+{
+	m_tid = sins_numparser::parse(val);
+}
+
+bool sinsp_filter_check_tid::run(sinsp_evt *evt)
+{
+	ASSERT(evt);
+
+	sinsp_threadinfo* tinfo = evt->get_thread_info();
+
+	if(tinfo != NULL && tinfo->m_tid == m_tid)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// sinsp_filter_expression implementation
+///////////////////////////////////////////////////////////////////////////////
+sinsp_filter_expression::sinsp_filter_expression()
+{
+	m_parent = NULL;
+}
+
+sinsp_filter_expression::~sinsp_filter_expression()
+{
+	uint32_t j;
+
+	for(j = 0; j < m_checks.size(); j++)
+	{
+		delete m_checks[j];
+	}
+}
+
+void sinsp_filter_expression::add_check(sinsp_filter_check* chk)
+{
+	m_checks.push_back(chk);
+}
+
+void sinsp_filter_expression::parse(string expr)
+{
+}
+	
+bool sinsp_filter_expression::run(sinsp_evt *evt)
+{
+	uint32_t j;
+	uint32_t size = m_checks.size();
+	bool res = true;
+	bool chkres;
+	 
+	for(j = 0; j < size; j++)
+	{
+		sinsp_filter_check* chk = m_checks[j];
+		ASSERT(chk != NULL);
+
+		chkres = chk->run(evt);
+		if(j == 0)
+		{
+			switch(chk->m_boolop)
+			{
+			case BO_NONE:
+				res = chkres;
+				break;
+			case BO_NOT:
+				res = !chkres;
+				break;
+			default:
+				ASSERT(false);
+				break;
+			}
+		}
+		else
+		{
+			switch(chk->m_boolop)
+			{
+			case BO_OR:
+				res = res || chkres;
+				break;
+			case BO_AND:
+				res = res && chkres;
+				break;
+			case BO_ORNOT:
+				res = res || !chkres;
+				break;
+			case BO_ANDNOT:
+				res = res && !chkres;
+				break;
+			default:
+				ASSERT(false);
+				break;
+			}
+		}
+	}
+
+	return res;
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// sinsp_filter implementation
+///////////////////////////////////////////////////////////////////////////////
+sinsp_filter::sinsp_filter(string fltstr)
+{
+//fltstr = "(comm ruby and tid 8976) or (comm rsyslogd and tid 393)";
+//fltstr = "(comm ruby and tid 8976)";
+//fltstr = "(comm ruby) and (tid 8976)";
+
+	m_scanpos = -1;
+	m_scansize = 0;
+	m_state = ST_READY_FOR_EXPRESSION;
+	m_curexpr = &m_filter;
+	m_last_boolop = BO_NONE;
+
+	parse(fltstr);
+}
+
+bool sinsp_filter::isblank(char c)
+{
+	if(c == ' ' || c == '\t' || c == '\n' || c == '\r')
+	{
+		return true;
+	}
+
+	return false;
+}
+
+char sinsp_filter::next()
+{
+	while(true)
+	{
+		m_scanpos++;
+
+		if(m_scanpos >= m_scansize)
+		{
+			return 0;
+		}
+
+		if(!isblank(m_fltstr[m_scanpos]))
+		{
+			return m_fltstr[m_scanpos];
+		}
+	}
+}
+
+string sinsp_filter::next_word()
+{
+	int32_t start;
+
+	//
+	// Skip spaces
+	//
+	if(isblank(m_fltstr[m_scanpos]))
+	{
+		next();
+	}
+
+	//
+	// Mark the beginning of the word
+	//
+	start = m_scanpos;
+
+	for(; m_scanpos < m_scansize; m_scanpos++)
+	{
+		char curchar = m_fltstr[m_scanpos];
+
+		if(isblank(curchar) ||
+			curchar == '(' ||
+			curchar == ')')
+		{
+			//
+			// End of word
+			//
+			ASSERT(m_scanpos > start);
+			string res = m_fltstr.substr(start, m_scanpos - start);
+
+			if(!isblank(curchar))
+			{
+				m_scanpos--;
+			}
+
+			return res;
+		}
+	}
+
+	//
+	// End of filter
+	//
+	return m_fltstr.substr(start, m_scansize - 1);
+}
+
+void sinsp_filter::parse_check(sinsp_filter_expression* parent_expr, boolop op)
+{
+	uint32_t startpos = m_scanpos;
+	string operand1 = next_word();
+	sinsp_filter_check* chk;
+
+	if(sinsp_filter_check_comm::recognize_operand(operand1))
+	{
+		sinsp_filter_check_comm* chk_comm = new sinsp_filter_check_comm();
+		chk = (sinsp_filter_check*)chk_comm;
+	}
+	else if(sinsp_filter_check_tid::recognize_operand(operand1))
+	{
+		sinsp_filter_check_tid* chk_tid = new sinsp_filter_check_tid();
+		chk = (sinsp_filter_check*)chk_tid;
+	}
+	else
+	{
+		throw sinsp_exception("filter error: unrecognized operand " + operand1 + " at pos " + to_string(startpos));
+	}
+
+	chk->parse_operand1(operand1);
+	string operand2 = next_word();
+	chk->parse_operand2(operand2);
+	chk->m_boolop = op;
+	parent_expr->add_check(chk);
+}
+
+void sinsp_filter::push_expression(boolop op)
+{
+	sinsp_filter_expression* newexpr = new sinsp_filter_expression();
+	newexpr->m_boolop = op;
+	newexpr->m_parent = m_curexpr;
+	m_last_boolop = BO_NONE;
+
+	m_curexpr->m_checks.push_back((sinsp_filter_check*)newexpr);
+	m_curexpr = newexpr;
+}
+
+void sinsp_filter::pop_expression()
+{
+	ASSERT(m_curexpr->m_parent != NULL);
+
+	m_curexpr = m_curexpr->m_parent;
+}
+
+void sinsp_filter::parse(string fltstr)
+{
+	m_fltstr = fltstr;
+	m_scansize = m_fltstr.size();
+
+	while(true)
+	{
+		char a = next();
+
+		switch(a)
+		{
+		case 0:
+			//
+			// Finished parsing the filter string
+			//
+			if(m_state == ST_READY_FOR_EXPRESSION)
+			{
+				return;
+			}
+			else
+			{
+				throw sinsp_exception("filter error: unexpected end of filter at position " + to_string(m_scanpos));
+			}
+
+			break;
+		case '(':
+			push_expression(m_last_boolop);
+			break;
+		case ')':
+			pop_expression();
+			break;
+		case 'o':
+			if(next() == 'r')
+			{
+				m_last_boolop = BO_OR;
+			}
+			else
+			{
+				throw sinsp_exception("syntax error in filter at position " + to_string(m_scanpos));
+			}
+
+			break;
+		case 'a':
+			if(next() == 'n' && next() == 'd')
+			{
+				m_last_boolop = BO_AND;
+			}
+			else
+			{
+				throw sinsp_exception("syntax error in filter at position " + to_string(m_scanpos));
+			}
+
+			break;
+		default:
+			parse_check(m_curexpr, m_last_boolop);
+			break;
+		}
+	}
+
+	vector<string> components = sinsp_split(m_fltstr, ' ');
+}
+
+bool sinsp_filter::run(sinsp_evt *evt)
+{
+	return m_filter.run(evt);
+}
