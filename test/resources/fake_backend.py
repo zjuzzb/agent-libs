524f43dbbe092a03946ea9f47a251a992434fec0
Gianluca Borello <gianluca@draios.com>
2013-09-20 14:27:59 +0200

    Added SSL tests by using a python fake backend local server

--- test/resources/fake_backend.py
+++ test/resources/fake_backend.py
@@ -2,7 +2,9 @@ import socket
 #import MySQLdb as mdb
 import sys
 import signal
+import ssl
 import time
+from optparse import OptionParser
 from struct import *
 
 def signal_handler(signal, frame):
@@ -10,6 +12,10 @@ def signal_handler(signal, frame):
   print 'You pressed Ctrl+C!'
   sys.exit(0)
   
+parser = OptionParser()
+parser.add_option("--ssl-key")
+parser.add_option("--ssl-certificate")
+(options, args) = parser.parse_args()
 
 PORT = 8080
 
@@ -29,17 +35,28 @@ while True:
   # Wait for a connection
   connect, address = s.accept()
 
+  sock = None
+
+  if options.ssl_certificate is None:
+    sock = connect
+  else:
+    sock = ssl.wrap_socket(connect,
+                           server_side=True,
+                           certfile=options.ssl_certificate,
+                           keyfile=options.ssl_key,
+                           ssl_version=ssl.PROTOCOL_TLSv1)
+
   while True:
     try:
       #time.sleep(1)
       #print "ciao"
       #continue
-      req = connect.recv(4)
+      req = sock.recv(4)
       msglen = socket.ntohl(unpack('I', req)[0])
       print msglen
-      req = connect.recv(msglen)
+      req = sock.recv(msglen)
       print len(req) 
-      #connect.close()
+      #sock.close()
       #s.close()
       #sys.exit(0)
     except Exception, e:
