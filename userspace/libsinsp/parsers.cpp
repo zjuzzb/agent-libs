ad0339625d61778c6457f6c32c4b0860e20eb54a
Loris Degioanni <loris@draios.com>
2013-10-08 14:14:11 -0700

    little fix in the filtering infrastructure: with syscalls that destroy the FD, make sure that the parser is run before the filter is applied

--- userspace/libsinsp/parsers.cpp
+++ userspace/libsinsp/parsers.cpp
@@ -52,7 +52,7 @@ void sinsp_parser::process_event(sinsp_evt *evt)
 	{
 		ppm_event_flags eflags = evt->get_flags();
 
-		if((eflags & EF_CREATES_FD) || 
+		if((eflags & (EF_CREATES_FD | EF_DESTROYS_FD)) || 
 			etype == PPME_SOCKET_CONNECT_X || etype == PPME_CLONE_X || etype == PPME_SYSCALL_EXECVE_X)
 		{
 			do_filter_later = true;
