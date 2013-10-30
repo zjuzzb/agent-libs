dabd1a2e46aa4883c30dcc75162616ec71492306
Loris Degioanni <loris@draios.com>
2013-10-29 21:33:02 -0700

    removed assertion

--- userspace/libsinsp/threadinfo.cpp
+++ userspace/libsinsp/threadinfo.cpp
@@ -691,10 +691,6 @@ void sinsp_thread_manager::increment_mainthread_childcount(sinsp_threadinfo& thr
 				++parent_thread->m_nchilds;
 			}
 		}
-		else
-		{
-			ASSERT(threadinfo.m_pid == 1 || threadinfo.m_ptid == -1);
-		}
 	}
 }
 
