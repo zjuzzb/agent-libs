a2d92bce3d0dc34df2b6b7ebd17dec16be3ecd40
Loris Degioanni <loris@draios.com>
2013-11-20 14:06:46 -0800

    more CPU info in the logs

--- userspace/libsinsp/sched_analyzer.cpp
+++ userspace/libsinsp/sched_analyzer.cpp
@@ -418,12 +418,13 @@ void sinsp_sched_analyzer2::flush(sinsp_evt* evt, uint64_t flush_time, bool is_e
 
 #if 1
 		g_logger.format(sinsp_logger::SEV_DEBUG, 
-			"***CPU %" PRIu32 " server:%" PRIu64 " other:%" PRIu64 " unknown:%" PRIu64 " idle:%" PRIu64 " idle1:%" PRIu32 " steal:%" PRIu32,
+			"***CPU %" PRIu32 " srv:%" PRIu64 " o:%" PRIu64 " u:%" PRIu64 " i:%" PRIu64 "(c:% " PRIu32 "i:%" PRIu32 " s:%" PRIu32 ")",
 			j,
 			state.m_lastsample_server_processes_ns,
 			state.m_lastsample_other_ns,
 			state.m_lastsample_unknown_ns,
 			state.m_lastsample_idle_ns,
+			(m_inspector->m_analyzer->m_cpu_idles.size() != 0)?m_inspector->m_analyzer->m_cpu_loads[j] : 0,
 			(m_inspector->m_analyzer->m_cpu_idles.size() != 0)?m_inspector->m_analyzer->m_cpu_idles[j] : 0,
 			(m_inspector->m_analyzer->m_cpu_steals.size() != 0)?m_inspector->m_analyzer->m_cpu_steals[j] : 0);
 #endif
