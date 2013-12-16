802ddb9d45fce266f18d9f25a224ff5a3c2240c3
Loris Degioanni <loris@draios.com>
2013-12-15 17:28:18 -0800

    cleanup

--- userspace/sdump/sdump.cpp
+++ userspace/sdump/sdump.cpp
@@ -206,8 +206,8 @@ int main(int argc, char **argv)
 
 	{
 		sinsp inspector;
-//		output_format = "*%evt.num)%evt.time.s.%evt.time.ns %evt.cpu %proc.name (%thread.tid) %evt.dir %evt.type %evt.args";
-		output_format = "%evt.num)%evt.type time:%latencyns";
+		output_format = "*%evt.num)%evt.time.s.%evt.time.ns %evt.cpu %proc.name (%thread.tid) %evt.dir %evt.type %evt.args";
+//		output_format = "%evt.num)%evt.type time:%latencyns";
 
 		//
 		// Parse the args
