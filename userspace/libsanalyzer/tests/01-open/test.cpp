476ced177f1e377810af0ae2735ac2339609a8c4
Gianluca Borello <gianluca@draios.com>
2014-01-15 10:55:00 -0800

    Fix includes

--- userspace/libsanalyzer/tests/01-open/test.cpp
+++ userspace/libsanalyzer/tests/01-open/test.cpp
@@ -3,7 +3,7 @@
 #include <stdio.h>
 #include <sinsp.h>
 #include <analyzer.h>
-#include "../../../libsinsp/settings.h"
+#include "settings.h"
 #include <iostream>
 #include <time.h>
 #include <signal.h>
