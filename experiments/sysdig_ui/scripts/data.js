80e3916e2c10fa96025fd4018779d7d110defe70
Loris Degioanni <loris@draios.com>
2014-04-17 09:39:56 -0700

    more interesting metrics in the sysdig UI

--- experiments/sysdig_ui/scripts/data.js
+++ experiments/sysdig_ui/scripts/data.js
@@ -3,6 +3,7 @@ var key_list_io = [
   {name:"Directory", field:"fd.directory", filter:""},
   {name:"Filename", field:"fd.name", filter:""},
   {name:"Process ID", field:"proc.pid", filter:""},
+  {name:"Process Arguments", field:"proc.args", filter:""},
   {name:"Process Name + Arguments", field:"proc.cmdline", filter:""},
   {name:"Thread ID", field:"thread.tid", filter:""},
   {name:"Username", field:"user.name", filter:""},
@@ -11,6 +12,7 @@ var key_list_io = [
   {name:"I/O direction", field:"evt.io_dir", filter:""},
   {name:"Error Code", field:"evt.res", filter:""},
   {name:"System call type", field:"evt.type", filter:""},
+  {name:"Executed command name", field:"evt.arg.exe", filter:""},
   {name:"None", field:"", filter:""},
 // cpu
 // in/out (for I/O)
@@ -29,29 +31,45 @@ var key_list_net = [
 // network protocol
 ];
 
+var key_list_syscall = [
+  {name:"System call type", field:"evt.type", filter:""},
+  {name:"Process Name", field:"proc.name", filter:""},
+  {name:"CPU", field:"evt.cpu", filter:""},
+  {name:"Filename", field:"fd.name", filter:""},
+  {name:"Process ID", field:"proc.pid", filter:""},
+  {name:"Process Name + Arguments", field:"proc.cmdline", filter:""},
+  {name:"Thread ID", field:"thread.tid", filter:""},
+  {name:"Username", field:"user.name", filter:""},
+  {name:"Parent Process Name", field:"proc.pname", filter:""},
+  {name:"Parent Process ID", field:"proc.ppid", filter:""},
+  {name:"Error Code", field:"evt.res", filter:""},
+  {name:"Executed command name", field:"evt.arg.exe", filter:""},
+  {name:"None", field:"", filter:""},
+];
+
 var value_list = [
   {name:"I/O Bytes", description:"amount of bytes read/written to disk", field:"evt.rawarg.res", filter:"fd.type=file and evt.is_io=true and evt.failed=false", keys: key_list_io},
   {name:"I/O Time", field:"evt.latency", filter:"fd.type=file and evt.is_io=true", keys: key_list_io},
   {name:"IOPS", field:"evt.count", filter:"fd.type=file and evt.is_io=true and evt.dir=< and evt.failed=false", keys: key_list_io},
   {name:"Disk I/O Failure Count", field:"evt.count", filter:"fd.type=file and evt.is_io=true and evt.dir=< and evt.failed=true", keys: key_list_io},
   {name:"File Open Failure Count", field:"evt.count", filter:"evt.type=open and evt.dir=< and evt.failed=true", keys: key_list_io},
-  {name:"System call Failure Count", field:"evt.count", filter:"evt.dir=< and evt.failed=true", keys: key_list_io},
+  {name:"> 1ms I/O calls count", field:"evt.count", filter:"fd.type=file and evt.latency > 1000000", keys: key_list_io},
+  {name:"> 10ms I/O calls count", field:"evt.count", filter:"fd.type=file and evt.latency > 10000000", keys: key_list_io},
+  {name:"> 100ms I/O calls count", field:"evt.count", filter:"fd.type=file and evt.latency > 100000000", keys: key_list_io},
   {name:"Network Bytes", description:"amount of bytes sent/received on the network", field:"evt.rawarg.res", filter:"fd.type=ipv4 and evt.is_io=true", keys: key_list_net},
   {name:"Incoming Connection Count", description:"number of received network connections", field:"evt.count", filter:"evt.type=accept and evt.dir=<", keys: key_list_net},
   {name:"Outgoing Connection Count", description:"number of attempted network connections", field:"evt.count", filter:"evt.type=connect and evt.dir=<", keys: key_list_net},
   {name:"Failed Connection Attempts", description:"number of falied network connection attempts", field:"evt.count", filter:"evt.type=connect and evt.dir=< and evt.failed=true", keys: key_list_net},
-// number of system calls
+  {name:"System Call Count", description:"number of system calls", field:"evt.count", filter:"evt.dir=<", keys: key_list_syscall},
+  {name:"System Call Time", description:"Time spent in system calls", field:"evt.latency", filter:"evt.dir=<", keys: key_list_syscall},
+  {name:"Failed System Call Count", description:"number of system calls that failed", field:"evt.count", filter:"evt.dir=< and evt.failed=true", keys: key_list_syscall},
+  {name:"# Executed Commands", description:"number of started processes", field:"evt.count", filter:"evt.dir=< and evt.type=execve", keys: key_list_syscall},
+// time breakdown (disk vs cpu vs net)
 // system call latency
-// failed system calls
 // cpu usage
-// failed I/O system calls
 // number of threads
 // number of FDs
-// I/O calls with latency bigger than 1ms
-// I/O calls with latency bigger than 10ms
-// I/O calls with latency bigger than 100ms
 // number of forks
-// number of executed commands
 // number of page faults
 ];
 
