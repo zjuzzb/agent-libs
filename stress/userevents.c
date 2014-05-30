2e5f2f2c70bcf422b2bbb30b8fb9ec035a0a55ed
Loris Degioanni <loris@draios.com>
2014-05-30 01:04:21 -0400

    app events parser tests. Still disabled.

--- stress/userevents.c
+++ stress/userevents.c
@@ -7,6 +7,7 @@
 #include <string.h>
 
 #define TMP_FILE_NAME "/tmp/sysdig_test.txt"
+#define BIGBUFSIZE 5000
 
 void generate_sysdig_event(FILE* f, char* text)
 {
@@ -23,21 +24,36 @@ int main(void)
 	clock_t begin = clock();
 	printf("%p\n", f);
 
-	for(j = 0; j < 1000; j++)
+	char* bigbuf = (char*)malloc(BIGBUFSIZE);
+	for(j = 0; j < BIGBUFSIZE; j++)
+	{
+		bigbuf[j] = '1' + (j % 5);
+	}
+	bigbuf[BIGBUFSIZE - 1] = 0;
+
+	for(j = 0; j < 5; j++)
 	{
 		FILE* tf1;
 
-		generate_sysdig_event(f, "1.0/>/test");
+//		generate_sysdig_event(f, bigbuf);
+/*
+		generate_sysdig_event(f, "[1, >, [\"lori,");
+		generate_sysdig_event(f, "sapp\",");
+		generate_sysdig_event(f, " \"loop\"], []]");
+*/		
+		generate_sysdig_event(f, "[1, >, [\"lorisapp\", \"loop\"], []]");
 
 		tf1 = fopen(TMP_FILE_NAME, "w");
-		generate_sysdig_event(f, "1.0/>/test/write");
+		generate_sysdig_event(f, "[2, >, [\"lorisapp\", \"loop\", \"write\"], [{\"argname1\":\"argval1\"}, {\"argname2\":\"argval2\"}]]");
 		fwrite("hello world", strlen("hello world"), 1, tf1);
-		generate_sysdig_event(f, "1.0/</test/write");
+		generate_sysdig_event(f, "[2, <, [], []]");
 		fclose(tf1);
 
 		unlink(TMP_FILE_NAME);
 
-		generate_sysdig_event(f, "1.0/</test");
+		generate_sysdig_event(f, "[1, <, [], []]");
+		
+		sleep(1);
 	}
 
 	clock_t end = clock();
---
+++ test/userevts.cpp
@@ -0,0 +1,694 @@
+#include <termios.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <sys/ioctl.h>
+#include <poll.h>
+#include <sys/resource.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+
+#define VISIBILITY_PRIVATE
+
+#include "sys_call_test.h"
+#include <gtest.h>
+#include <algorithm>
+#include "event_capture.h"
+#include <sys/stat.h>
+#include <Poco/Process.h>
+#include <Poco/PipeStream.h>
+#include <list>
+#include <cassert>
+#include <event.h>
+#include <Poco/StringTokenizer.h>
+#include <Poco/NumberFormatter.h>
+#include <Poco/NumberParser.h>
+
+#include <sinsp.h>
+#include <sinsp_int.h>
+#include <parsers.h>
+#include "appevts.h"
+
+using namespace std;
+
+using Poco::StringTokenizer;
+using Poco::NumberFormatter;
+using Poco::NumberParser;
+
+#if 0
+
+TEST_F(sys_call_test, usrevts_1)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[12435, >, [\"mysql\", \"query\", \"init\"], [{\"argname1\":\"argval1\"}, {\"argname2\":\"argval2\"}, {\"argname3\":\"argval3\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_EQ(true, p.m_is_enter);
+	EXPECT_EQ("12435", string(p.m_id));
+	EXPECT_EQ(3, (int)p.m_tags.size());
+	EXPECT_EQ(3, (int)p.m_argnames.size());
+	EXPECT_EQ(3, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_2)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[, <, [\"mysql\"], [{\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_NE(true, p.m_is_enter);
+	EXPECT_EQ("", string(p.m_id));
+	EXPECT_EQ(1, (int)p.m_tags.size());
+	EXPECT_EQ(1, (int)p.m_argnames.size());
+	EXPECT_EQ(1, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_3)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, a, [\"mysql\"], [{\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_4)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123 >, [\"mysql\"], [{\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_5)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, > [\"mysql\"], [{\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_6)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, [\"mysql\"] [{\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_7)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, \"mysql\"], [{\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_8)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, [\"mysql\", [{\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_9)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, \"mysql\", [{\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_10)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[[123], >, [\"mysql\"], [{\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_11)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[[123, >, [\"mysql\"], [{\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_12)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, [\"mysql\"], [\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_13)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, [\"mysql\"], [{\"argname1\":\"argval1\"]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_14)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, [\"mysql\"], [\"argname1\":\"argval1\"]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_15)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, [\"mysql\"], [{\"argname1\":\"argval1\"}, {\"argname1\":\"argval1\"]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_16)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, [\"mysql\"], [{\"argname1\":\"argval1\"}, \"argname1\":\"argval1\"]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_17)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, [mysql], [{\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_18)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, [\"mysql\"], [{argname1:\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_19)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, [\"mysql\"], [{\"argname1\":argval1}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_20)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, [\"mysql\"], [{\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_21)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, [\"mysql\"], [{\"argname1\":\"argval1\"}, {\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_22)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, [\"mysql\"], [{\"argname1\":\"argval1\"} {\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_23)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[123, >, [\"mysql\"], [{\"argname1\":\"argval1\"}{\"argname1\":\"argval1\"}]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_24)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[12, <, [\"mysql\"], []]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_NE(true, p.m_is_enter);
+	EXPECT_EQ("12", string(p.m_id));
+	EXPECT_EQ(1, (int)p.m_tags.size());
+	EXPECT_EQ(0, (int)p.m_argnames.size());
+	EXPECT_EQ(0, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_25)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[12, <, [], [{\"argname1\":\"argval1\"}, {\"argname2\":\"argval2\"}]]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_NE(true, p.m_is_enter);
+	EXPECT_EQ("12", string(p.m_id));
+	EXPECT_EQ(0, (int)p.m_tags.size());
+	EXPECT_EQ(2, (int)p.m_argnames.size());
+	EXPECT_EQ(2, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_26)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[12, <, [], []]]";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_NE(true, p.m_is_enter);
+	EXPECT_EQ("12", string(p.m_id));
+	EXPECT_EQ(0, (int)p.m_tags.size());
+	EXPECT_EQ(0, (int)p.m_argnames.size());
+	EXPECT_EQ(0, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_27)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "        [12, <, [], []]]        ";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_NE(true, p.m_is_enter);
+	EXPECT_EQ("12", string(p.m_id));
+	EXPECT_EQ(0, (int)p.m_tags.size());
+	EXPECT_EQ(0, (int)p.m_argnames.size());
+	EXPECT_EQ(0, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_28)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "        [ 12  ,        <        , [], [  ]    ]         ]        ";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_NE(true, p.m_is_enter);
+	EXPECT_EQ("12", string(p.m_id));
+	EXPECT_EQ(0, (int)p.m_tags.size());
+	EXPECT_EQ(0, (int)p.m_argnames.size());
+	EXPECT_EQ(0, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_29)
+{
+	sinsp_usrevtparser p;
+
+	char doc[] = "[12, ";
+	char buffer[sizeof(doc)];
+	memcpy(buffer, doc, sizeof(doc));
+
+	p.process_event_data(buffer, sizeof(doc) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+}
+
+TEST_F(sys_call_test, usrevts_31)
+{
+	sinsp_usrevtparser p;
+
+	char doc1[] = "[12435, >, [\"mysql\", \"query\", \"in";
+	char doc2[] = "it\"], [{\"argname1\":\"argval1\"}, {\"argname2\":\"argval2\"";
+	char doc3[] = "}, {\"argname3\":\"argval3\"}]]";
+
+	p.process_event_data(doc1, sizeof(doc1) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc2, sizeof(doc2) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc3, sizeof(doc3) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_EQ(true, p.m_is_enter);
+	EXPECT_EQ("12435", string(p.m_id));
+	EXPECT_EQ(3, (int)p.m_tags.size());
+	EXPECT_EQ(3, (int)p.m_argnames.size());
+	EXPECT_EQ(3, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_32)
+{
+	sinsp_usrevtparser p;
+
+	char doc1[] = "[12435, >, [\"mysql\", \"query\"";
+	char doc2[] = ", \"init\"], [{\"argname1\":\"argval1\"}, {\"argname2\":";
+	char doc3[] = "\"argval2\"}, {\"argname3\":\"argval3\"}]]";
+
+	p.process_event_data(doc1, sizeof(doc1) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc2, sizeof(doc2) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc3, sizeof(doc3) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_EQ(true, p.m_is_enter);
+	EXPECT_EQ("12435", string(p.m_id));
+	EXPECT_EQ(3, (int)p.m_tags.size());
+	EXPECT_EQ(3, (int)p.m_argnames.size());
+	EXPECT_EQ(3, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_33)
+{
+	sinsp_usrevtparser p;
+
+	char doc1[] = "[12435, >, [\"mys";
+	char doc2[] = "ql\", \"query\", \"init\"], [{\"argname1\":\"argval1\"}";
+	char doc3[] = ", {\"argname2\":\"argval2\"}, {\"argname3\":\"argval3\"}]]";
+
+	p.process_event_data(doc1, sizeof(doc1) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc2, sizeof(doc2) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc3, sizeof(doc3) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_EQ(true, p.m_is_enter);
+	EXPECT_EQ("12435", string(p.m_id));
+	EXPECT_EQ(3, (int)p.m_tags.size());
+	EXPECT_EQ(3, (int)p.m_argnames.size());
+	EXPECT_EQ(3, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_34)
+{
+	sinsp_usrevtparser p;
+
+	char doc1[] = "[12435, >,";
+	char doc2[] = " [\"mysql\", \"query\", ";
+	char doc3[] = "\"init\"], [{\"argname1\":\"argval1\"}, {\"argname2\":\"argval2\"}, {\"argname3\":\"argval3\"}]]";
+
+	p.process_event_data(doc1, sizeof(doc1) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc2, sizeof(doc2) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc3, sizeof(doc3) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_EQ(true, p.m_is_enter);
+	EXPECT_EQ("12435", string(p.m_id));
+	EXPECT_EQ(3, (int)p.m_tags.size());
+	EXPECT_EQ(3, (int)p.m_argnames.size());
+	EXPECT_EQ(3, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_35)
+{
+	sinsp_usrevtparser p;
+
+	char doc1[] = "[12435, >";
+	char doc2[] = ", [\"mysql\", \"query\",";
+	char doc3[] = " \"init\"], [{\"argname1\":\"argval1\"}, {\"argname2\":\"argval2\"}, {\"argname3\":\"argval3\"}]]";
+
+	p.process_event_data(doc1, sizeof(doc1) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc2, sizeof(doc2) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc3, sizeof(doc3) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_EQ(true, p.m_is_enter);
+	EXPECT_EQ("12435", string(p.m_id));
+	EXPECT_EQ(3, (int)p.m_tags.size());
+	EXPECT_EQ(3, (int)p.m_argnames.size());
+	EXPECT_EQ(3, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_36)
+{
+	sinsp_usrevtparser p;
+
+	char doc1[] = "[1243";
+	char doc2[] = "5, >, [\"mysql\", \"que";
+	char doc3[] = "ry\", \"init\"], [{\"argname1\":\"argval1\"}, {\"argname2\":\"argval2\"}, {\"argname3\":\"argval3\"}]]";
+
+	p.process_event_data(doc1, sizeof(doc1) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc2, sizeof(doc2) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc3, sizeof(doc3) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_EQ(true, p.m_is_enter);
+	EXPECT_EQ("12435", string(p.m_id));
+	EXPECT_EQ(3, (int)p.m_tags.size());
+	EXPECT_EQ(3, (int)p.m_argnames.size());
+	EXPECT_EQ(3, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_37)
+{
+	sinsp_usrevtparser p;
+
+	char doc1[] = "[";
+	char doc2[] = "1243";
+	char doc3[] = "5, >, [\"mysql\", \"query\", \"init\"], [{\"argname1\":\"argval1\"}, {\"argname2\":\"argval2\"}, {\"argname3\":\"argval3\"}]]";
+
+	p.process_event_data(doc1, sizeof(doc1) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc2, sizeof(doc2) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc3, sizeof(doc3) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_EQ(true, p.m_is_enter);
+	EXPECT_EQ("12435", string(p.m_id));
+	EXPECT_EQ(3, (int)p.m_tags.size());
+	EXPECT_EQ(3, (int)p.m_argnames.size());
+	EXPECT_EQ(3, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_38)
+{
+	sinsp_usrevtparser p;
+
+	char doc1[] = "";
+	char doc2[] = "[1243";
+	char doc3[] = "5, >, [\"mysql\", \"query\", \"init\"], [{\"argname1\":\"argval1\"}, {\"argname2\":\"argval2\"}, {\"argname3\":\"argval3\"}]]";
+
+	p.process_event_data(doc1, sizeof(doc1) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+
+	p.process_event_data(doc2, sizeof(doc2) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+
+	p.process_event_data(doc3, sizeof(doc3) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_EQ(true, p.m_is_enter);
+	EXPECT_EQ("12435", string(p.m_id));
+	EXPECT_EQ(3, (int)p.m_tags.size());
+	EXPECT_EQ(3, (int)p.m_argnames.size());
+	EXPECT_EQ(3, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_39)
+{
+	sinsp_usrevtparser p;
+
+	char doc1[] = "";
+	char doc2[] = "";
+	char doc3[] = "[12435, >, [\"mysql\", \"query\", \"init\"], [{\"argname1\":\"argval1\"}, {\"argname2\":\"argval2\"}, {\"argname3\":\"argval3\"}]]";
+
+	p.process_event_data(doc1, sizeof(doc1) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+
+	p.process_event_data(doc2, sizeof(doc2) - 1);
+	EXPECT_EQ(sinsp_usrevtparser::RES_FAILED, p.m_res);
+
+	p.process_event_data(doc3, sizeof(doc3) - 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_EQ(true, p.m_is_enter);
+	EXPECT_EQ("12435", string(p.m_id));
+	EXPECT_EQ(3, (int)p.m_tags.size());
+	EXPECT_EQ(3, (int)p.m_argnames.size());
+	EXPECT_EQ(3, (int)p.m_argvals.size());
+}
+
+TEST_F(sys_call_test, usrevts_40)
+{
+	sinsp_usrevtparser p;
+	char tb[2];
+	tb[1] = 0;
+	uint32_t k;
+
+	char doc[] = "[12435, >, [\"mysql\", \"query\", \"init\"], [{\"argname1\":\"argval1\"}, {\"argname2\":\"argval2\"}, {\"argname3\":\"argval3\"}]]";
+
+	for(k = 0; k < sizeof(doc) - 2; k++)
+	{
+		tb[0] = doc[k];
+		p.process_event_data(tb, 1);
+		EXPECT_EQ(sinsp_usrevtparser::RES_TRUNCATED, p.m_res);
+	}
+
+	tb[0] = doc[k];
+	p.process_event_data(tb, 1);
+
+	EXPECT_EQ(sinsp_usrevtparser::RES_OK, p.m_res);
+	EXPECT_EQ(true, p.m_is_enter);
+	EXPECT_EQ("12435", string(p.m_id));
+	EXPECT_EQ(3, (int)p.m_tags.size());
+	EXPECT_EQ(3, (int)p.m_argnames.size());
+	EXPECT_EQ(3, (int)p.m_argvals.size());
+}
+
+#endif // 0
