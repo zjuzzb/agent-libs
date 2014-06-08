d9e00b453cce015964354395ef7ff4cab720a848
Loris Degioanni <loris@draios.com>
2014-06-08 16:06:42 -0700

    refactory of the multitable chisel into a more generic, object oriented datacube tool. Still under experiments because it needs more work before going into sysdig.

---
+++ experiments/datacube_chisel/datacube.lua
@@ -0,0 +1,137 @@
+--[[
+Copyright (C) 2013-2014 Draios inc.
+ 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.
+
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+
+--[[ 
+This file implements a multidimensional table
+]]--
+
+json = require ("dkjson")
+
+local datacube = {}
+
+function datacube.set_viz_info(viz_info)
+	datacube.viz_info = viz_info
+end
+
+function datacube.insert(keys, key_deflts, tbl, value, depth)
+	local key = evt.field(keys[depth])
+	
+	if key == nil then
+		if key_deflts ~= nil then
+			key = key_deflts[depth]
+		end
+	end
+
+	if key == nil then
+		return
+	end
+	
+	local entryval = tbl[key]
+
+	if entryval == nil then
+		tbl[key] = {{}, value}
+
+		if depth < #keys then
+			insert(keys, tbl[key][1], value, depth + 1)
+		end
+	else
+		tbl[key][2] = tbl[key][2] + value
+		
+		if depth < #keys then
+			insert(keys, tbl[key][1], value, depth + 1)
+		end
+	end	
+end
+
+function datacube.print_table_normal(tbl, timedelta, depth)
+	local sorted_grtable = pairs_top_by_val(tbl.top_number, function(t,a,b) return t[b][2] < t[a][2] end)
+	
+	for k,v in sorted_grtable do
+		if datacube.viz_info.valueunits[1] == "none" then
+			print(extend_string("", depth - 1) .. extend_string(v[2], 10) .. k)
+		elseif datacube.viz_info.valueunits[1] == "bytes" then
+			print(extend_string(format_bytes(v[2]), 10) .. k)
+		elseif datacube.viz_info.valueunits[1] == "time" then
+			print(extend_string(format_time_interval(v[2]), 10) .. k)
+		elseif datacube.viz_info.valueunits[1] == "timepct" then
+			if timedelta ~= 0 then
+				pctstr = string.format("%.2f%%", v[2] / timedelta * 100)
+			else
+				pctstr = "0.00%"
+			end
+
+			print(extend_string(pctstr, 10) .. k)
+		end
+		
+		if depth < #datacube.viz_info.key_fld then
+			print_table_normal(v[1], timedelta, depth + 1)
+		end
+	end
+end
+
+function datacube.create_json_table(input_tbl, timedelta, depth)
+	local sorted_grtable = pairs_top_by_val(input_tbl, datacube.viz_info.top_number, function(t,a,b) return t[b][2] < t[a][2] end)
+	
+	local j = 1
+	local res = {}
+		
+	for k,v in sorted_grtable do
+		local entry = {}
+		entry.name = k
+		res[j] = entry
+		j = j + 1
+
+		if depth < #datacube.viz_info.key_fld then
+			entry.children = datacube.create_json_table(v[1], timedelta, depth + 1)
+		else
+			entry.value = v[2]
+		end
+	end
+		
+	return res
+end
+
+function datacube.table_to_json_string(stable, ts_s, ts_ns, timedelta)		
+	local jtable = {}
+	jtable.children = datacube.create_json_table(stable, timedelta, 1)
+	jtable.name = "root"
+
+	return json.encode(jtable, { indent = true })
+end
+
+function datacube.print(stable, ts_s, ts_ns, timedelta)
+	if datacube.viz_info.output_format == "json" then
+		local str = datacube.table_to_json_string(stable, ts_s, ts_ns, timedelta)
+		print(str)
+	else
+		header = ""
+		for i, name in ipairs(datacube.viz_info.value_desc) do
+			header = header .. extend_string(name, 10)
+		end
+
+		for i, name in ipairs(datacube.viz_info.key_desc) do
+			header = header .. extend_string(name, 20)
+		end
+
+		print(header)
+		print("------------------------------")
+		
+		print_table_normal(stable, timedelta, 1)
+	end
+end
+
+return datacube
---
+++ experiments/datacube_chisel/multitable.lua
@@ -0,0 +1,299 @@
+--[[
+Copyright (C) 2013-2014 Draios inc.
+ 
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License version 2 as
+published by the Free Software Foundation.
+
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+--]]
+
+-- Chisel description
+description = "multidimensional table generator."
+short_description = "multidimensional table generator"
+category = "IO"
+hidden = true
+
+-- Chisel argument list
+args = 
+{
+	{
+		name = "keys", 
+		description = "comma-separated list of filter fields to use for grouping", 
+		argtype = "string"
+	},
+	{
+		name = "keydescs", 
+		description = "comma separated list of human readable descriptions for the key", 
+		argtype = "string"
+	},
+	{
+		name = "keydefaults", 
+		description = "comma separated list of default values for keys. If not specified or -, a default value is used when the key is not present", 
+		argtype = "string"
+	},
+	{
+		name = "values", 
+		description = "comma separated list of values to count for every key", 
+		argtype = "string"
+	},
+	{
+		name = "valuedescs", 
+		description = "comma separated list of human readable descriptions for the values", 
+		argtype = "string"
+	},
+	{
+		name = "valueoperations", 
+		description = "comma separated list of operations to apply to values. Valid operations are 'SUM', 'AVG', 'MIN', 'MAX'.", 
+		argtype = "string"
+	},
+	{
+		name = "valueunits", 
+		description = "how to render the values in the result. Can be 'bytes', 'time', 'timepct', or 'none'.", 
+		argtype = "string"
+	},
+	{
+		name = "valuedefaults", 
+		description = "comma separated list of default values for keys. If not specified or -, a default value is used when the value is not present.", 
+		argtype = "string"
+	},
+	{
+		name = "filter", 
+		description = "the filter to apply", 
+		argtype = "string"
+	},
+	{
+		name = "top_number", 
+		description = "maximum number of elements to display", 
+		argtype = "string"
+	},
+	{
+		name = "do_diff", 
+		description = "'true' if the script should perform a diff among the two input trace files, 'false' otherwise.", 
+		argtype = "bool"
+	},
+}
+
+require "common"
+dcube = require "datacube"
+require "deltas"
+terminal = require "ansiterminal"
+
+grtable = {}
+filter = ""
+islive = false
+fkeys = {}
+run_cnt = 0
+
+vizinfo = 
+{
+	key_fld = {},
+	key_desc = {},
+	key_defaults = nil,
+	value_fld = {},
+	value_desc = {},
+	value_operations = {},
+	value_defaults = nil,
+	valueunits = {},
+	top_number = 0,
+	output_format = "normal",
+	do_diff = false
+}
+
+-- Argument notification callback
+function on_set_arg(name, val)
+	if name == "keys" then
+		vizinfo.key_fld = split(val, ",")		
+		return true
+	elseif name == "keydescs" then
+		vizinfo.key_desc = split(val, ",")
+		return true
+	elseif name == "keydefaults" then
+		if val ~= "-" and val ~= "" then
+			vizinfo.key_defaults = split(val, ",")
+
+			if #vizinfo.key_fld ~= #vizinfo.key_defaults then
+				print("error: number of entries in keys different from number entries in keydefaults")
+				return false
+			end
+		
+			local noneset = true
+			for i, v in ipairs(vizinfo.key_defaults) do
+				if vizinfo.key_defaults[i] == "" then
+					vizinfo.key_defaults[i] = nil
+				else
+					noneset = false
+				end
+			end
+			
+			if noneset then
+				vizinfo.key_defaults = nil
+			end
+		end
+					
+		return true
+	elseif name == "values" then
+		vizinfo.value_fld = split(val, ",")
+		return true
+	elseif name == "valuedescs" then
+		vizinfo.value_desc = split(val, ",")
+		return true
+	elseif name == "valueoperations" then
+		vizinfo.value_operations = split(val, ",")
+		return true
+	elseif name == "valueunits" then
+		vizinfo.valueunits = split(val, ",")
+		return true
+	elseif name == "valuedefaults" then
+		if val ~= "-" and val ~= "" then
+			vizinfo.value_defaults = split(val, ",")
+		end
+		return true
+	elseif name == "filter" then
+		if val ~= "-" and val ~= "" then
+			filter = val
+		end
+		return true
+	elseif name == "top_number" then
+		vizinfo.top_number = tonumber(val)
+		return true
+	elseif name == "do_diff" then
+		if val == 'true' then 
+			vizinfo.do_diff = true
+		end
+		return true
+	end
+
+	return false
+end
+
+function on_init()
+	run_cnt = run_cnt + 1
+	
+	dcube.set_viz_info(vizinfo)
+
+	if #vizinfo.key_fld ~= #vizinfo.key_desc then
+		print("error: number of entries in keys different from number entries in keydescs")
+		return false
+	end
+	
+	if #vizinfo.value_fld ~= #vizinfo.value_desc then
+		print("error: number of entries in values different from number entries in valuedescs")
+		return false
+	end
+	
+	if #vizinfo.value_fld ~= #vizinfo.value_operations then
+		print("error: number of entries in values different from number entries in valueoperations")
+		return false
+	end
+	
+	if #vizinfo.value_fld ~= #vizinfo.valueunits then
+		print("error: number of entries in values different from number entries in valueunits")
+		return false
+	end
+	
+	if vizinfo.value_defaults ~= nil then
+		if #vizinfo.value_fld ~= #vizinfo.value_defaults then
+			print("error: number of entries in values different from number entries in valuedefaults")
+			return false
+		end
+	end
+		
+	-- Request the fields we need
+	for i, name in ipairs(vizinfo.key_fld) do
+		fkeys[i] = chisel.request_field(name)
+	end
+
+	fvalue = chisel.request_field(vizinfo.value_fld[1])
+
+	-- set the filter
+	if filter ~= "" then
+		chisel.set_filter(filter)
+	end
+	
+	return true
+end
+
+function on_capture_start()
+	islive = sysdig.is_live()
+	vizinfo.output_format = sysdig.get_output_format()
+
+	if islive then
+		chisel.set_interval_s(1)
+		if vizinfo.output_format ~= "json" then
+			terminal.clearscreen()
+			terminal.hidecursor()
+		end
+	end
+	
+	return true
+end
+
+function on_event()
+	local value = evt.field(fvalue)
+
+	if value ~= nil then
+		dcube.insert(fkeys, vizinfo.key_defaults, grtable, value, 1)
+	else
+		if vizinfo.value_defaults ~= nil then 
+			dcube.insert(keys, grtable, vizinfo.value_defaults[1], 1)
+		end
+	end
+	
+	return true
+end
+
+function on_interval(ts_s, ts_ns, delta)	
+	if vizinfo.output_format ~= "json" then
+		terminal.clearscreen()
+		terminal.goto(0, 0)
+	end
+	
+	dcube.print(grtable, ts_s, 0, delta, vizinfo)
+
+	-- Clear the table
+	grtable = {}
+	
+	return true
+end
+
+function on_capture_end(ts_s, ts_ns, delta)
+	if islive and vizinfo.output_format ~= "json" then
+		terminal.clearscreen()
+		terminal.goto(0 ,0)
+		terminal.showcursor()
+		return true
+	end
+
+	if vizinfo.do_diff then
+		if run_cnt == 1 then
+
+			-- t1 is global because we use it at the next run
+			t1 = {}
+			t1.children = create_json_table(grtable, delta, vizinfo, 1)
+			t1.name = "root"
+			t1.timedelta = delta
+
+			grtable = {}
+		else
+			local t2 = {}
+
+			t2.children = create_json_table(grtable, delta, vizinfo, 1)
+			t2.name = "root"
+			t2.timedelta = delta
+			
+			print_table_difference(t1, t2, vizinfo)
+		end
+	else
+		dcube.print(grtable, ts_s, 0, delta, vizinfo)
+	end
+	
+	return true
+end
