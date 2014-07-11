ce744de5c6c667ef9314d51cccc4430a224bf316
Gianluca Borello <gianluca@draios.com>
2014-07-11 20:50:43 +0200

    Lower log verbosity for release builds

--- userspace/dragent/configuration.cpp
+++ userspace/dragent/configuration.cpp
@@ -117,8 +117,7 @@ void dragent_configuration::init(Application* app)
 #ifdef _DEBUG
 		m_min_file_priority = string_to_priority(config.getString("logpriority.file", "debug"));
 #else
-//		m_min_file_priority = string_to_priority(config.getString("logpriority.file", "info"));
-		m_min_file_priority = string_to_priority(config.getString("logpriority.file", "debug"));
+		m_min_file_priority = string_to_priority(config.getString("logpriority.file", "info"));
 #endif		
 	}
 
