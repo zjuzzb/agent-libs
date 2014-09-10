d79fbfc6476684e3281df1c95979b0c6f657d53d
Loris Degioanni <loris@draios.com>
2014-09-09 22:06:14 -0700

    bugfix: the URL list was not properly included in the protobuf

--- userspace/libsanalyzer/protostate.cpp
+++ userspace/libsanalyzer/protostate.cpp
@@ -21,12 +21,13 @@ void sinsp_protostate::to_protobuf(draiosproto::proto_info* protobuf_msg, uint32
 {
 	unordered_map<string, sinsp_url_details>::iterator uit;
 
+
 	if(m_server_urls.size() != 0)
 	{
-		draiosproto::url_details* ud = protobuf_msg->mutable_http()->add_server_urls();
-
 		for(uit = m_server_urls.begin(); uit != m_server_urls.end(); ++uit)
 		{
+			draiosproto::url_details* ud = protobuf_msg->mutable_http()->add_server_urls();
+
 			ud->set_url(uit->first);
 			ud->set_ncalls(uit->second.m_ncalls);
 			ud->set_time_tot(uit->second.m_time_tot);
@@ -37,10 +38,10 @@ void sinsp_protostate::to_protobuf(draiosproto::proto_info* protobuf_msg, uint32
 
 	if(m_client_urls.size() != 0)
 	{
-		draiosproto::url_details* ud = protobuf_msg->mutable_http()->add_client_urls();
-
 		for(uit = m_client_urls.begin(); uit != m_client_urls.end(); ++uit)
 		{
+			draiosproto::url_details* ud = protobuf_msg->mutable_http()->add_client_urls();
+
 			ud->set_url(uit->first);
 			ud->set_ncalls(uit->second.m_ncalls);
 			ud->set_time_tot(uit->second.m_time_tot);
