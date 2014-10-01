93f0fe0d9cd36875161671731311dd9856d052a2
Loris Degioanni <loris@draios.com>
2014-09-30 19:31:14 -0700

    gcc compile error

--- userspace/libsanalyzer/parser_mysql.cpp
+++ userspace/libsanalyzer/parser_mysql.cpp
@@ -116,7 +116,7 @@ bool sinsp_mysql_parser::parse_request(char* buf, uint32_t buflen)
 
 					if(tbuf < bufend)
 					{
-						char* db = (caps & CAP_CONNECT_WITH_DB ? tbuf : "<NA>");
+						char* db = (caps & CAP_CONNECT_WITH_DB ? tbuf : (char*)"<NA>");
 
 						m_database = m_storage.strcopy(db, bufend - tbuf);
 
