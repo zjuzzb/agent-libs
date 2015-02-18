f71e5602c1ecb1cd2ee2192da8c9f3c38eff5c22
Gianluca Borello <gianluca@draios.com>
2015-02-18 15:53:28 -0800

    progress on the dockerfile: explicit version for trusted builds and conversion from environment to access key and tags

---
+++ scripts/docker/docker-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#set -e
+
+if [ ! -z "$ACCESS_KEY" ]; then
+	echo "* Setting access key"
+	
+	if ! grep ^customerid /opt/draios/bin/dragent.properties > /dev/null 2>&1; then
+		echo "customerid = $ACCESS_KEY" >> /opt/draios/bin/dragent.properties
+	else
+		sed -i "s/^customerid.*/customerid = $ACCESS_KEY/g" /opt/draios/bin/dragent.properties
+	fi
+fi
+
+if [ ! -z "$TAGS" ]; then
+	echo "* Setting tags"
+
+	if ! grep ^ui.tags /opt/draios/bin/dragent.properties > /dev/null 2>&1; then
+		echo "ui.tags = $TAGS" >> /opt/draios/bin/dragent.properties
+	else
+		sed -i "s/^ui\.tags.*/ui.tags = $TAGS/g" /opt/draios/bin/dragent.properties
+	fi
+fi
+
+exec "$@"
