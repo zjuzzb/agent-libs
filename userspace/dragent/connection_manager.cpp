21c2059e144767ac591340a088cbac6d20c9a176
Gianluca Borello <gianluca@draios.com>
2015-03-16 17:42:02 -0700

    Reduce reconnection interval

--- userspace/dragent/connection_manager.cpp
+++ userspace/dragent/connection_manager.cpp
@@ -8,7 +8,7 @@
 
 const string connection_manager::m_name = "connection_manager";
 const chrono::seconds connection_manager::WORKING_INTERVAL_S(10);
-const uint32_t connection_manager::RECONNECT_MAX_INTERVAL_S(300);
+const uint32_t connection_manager::RECONNECT_MAX_INTERVAL_S(60);
 
 connection_manager::connection_manager(dragent_configuration* configuration, 
 		protocol_queue* queue, sinsp_worker* sinsp_worker):
