a53a518dafd3e965e0f556aed650b40701a84824
Luca Marturana <luca@sysdig.com>
2015-06-22 20:16:56 +0200

    Add compatibility with glib 2.12

--- userspace/sdjagent/native/com_sysdigcloud_sdjagent_CLibrary.cpp
+++ userspace/sdjagent/native/com_sysdigcloud_sdjagent_CLibrary.cpp
@@ -127,6 +127,21 @@ const char* scap_get_host_root()
 	return p;
 }
 
+// Use raw setns syscall for versions of glibc that don't include it (namely glibc-2.12)
+#if __GLIBC__ == 2 && __GLIBC_MINOR__ < 14
+#define _GNU_SOURCE
+#include "syscall.h"
+#if defined(__NR_setns) && !defined(SYS_setns)
+#define SYS_setns __NR_setns
+#endif
+#ifdef SYS_setns
+int setns(int fd, int nstype)
+{
+	return syscall(SYS_setns, fd, nstype);
+}
+#endif
+#endif
+
 JNIEXPORT jint JNICALL Java_com_sysdigcloud_sdjagent_CLibrary_real_1seteuid
         (JNIEnv *, jclass, jlong euid)
 {
