5a049c7c6b2239b71f55dac49cfdead32e392397
Luca Marturana <luca@sysdig.com>
2015-06-23 11:45:49 +0200

    Fix indentation

--- userspace/sdjagent/native/com_sysdigcloud_sdjagent_CLibrary.cpp
+++ userspace/sdjagent/native/com_sysdigcloud_sdjagent_CLibrary.cpp
@@ -145,21 +145,21 @@ int setns(int fd, int nstype)
 JNIEXPORT jint JNICALL Java_com_sysdigcloud_sdjagent_CLibrary_real_1seteuid
         (JNIEnv *, jclass, jlong euid)
 {
-    int res = seteuid(euid);
-    // We need to call again prctl() because PDEATHSIG is cleared
-    // after seteuid call
-    prctl(PR_SET_PDEATHSIG, SIGKILL);
-    return res;
+	int res = seteuid(euid);
+	// We need to call again prctl() because PDEATHSIG is cleared
+	// after seteuid call
+	prctl(PR_SET_PDEATHSIG, SIGKILL);
+	return res;
 }
 
 JNIEXPORT jint JNICALL Java_com_sysdigcloud_sdjagent_CLibrary_real_1setegid
         (JNIEnv *, jclass, jlong egid)
 {
-    int res = setegid(egid);
-    // We need to call again prctl() because PDEATHSIG is cleared
-    // after setegid call
-    prctl(PR_SET_PDEATHSIG, SIGKILL);
-    return res;
+	int res = setegid(egid);
+	// We need to call again prctl() because PDEATHSIG is cleared
+	// after setegid call
+	prctl(PR_SET_PDEATHSIG, SIGKILL);
+	return res;
 }
 
 JNIEXPORT jint JNICALL Java_com_sysdigcloud_sdjagent_CLibrary_real_1setenv
