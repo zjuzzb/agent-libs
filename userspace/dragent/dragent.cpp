efebb678cdcde1293fa87647eb5a6c28470bd85a
Luca Marturana <luca@sysdig.com>
2015-07-07 12:47:56 +0200

    remove comment

--- userspace/dragent/dragent.cpp
+++ userspace/dragent/dragent.cpp
@@ -308,7 +308,6 @@ int dragent_app::main(const std::vector<std::string>& args)
 	m_sdchecks_pipes = make_unique<errpipe_manager>();
 	m_subprocesses_logger.add_logfd(m_sdchecks_pipes->get_file(), [](const string& line)
 	{
-		// TODO: Add log level parsing
 		auto parsed_log = sinsp_split(line, ':');
 		if(parsed_log.size() >= 3 && isdigit(parsed_log.at(0).at(0)))
 		{
