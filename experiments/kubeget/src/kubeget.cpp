4e20347bd0cfbd45da7f3ac2ee4a33a195183e4b
Alex Fabijanic <alex@pocoproject.org>
2015-10-19 20:12:03 -0500

    replaced Poco::URI, watch logic fixes, coding style fixes, watch NOT thread-safe

--- experiments/kubeget/src/kubeget.cpp
+++ experiments/kubeget/src/kubeget.cpp
@@ -32,7 +32,7 @@ int main(int argc, char** argv)
 	{
 		std::string host("http://localhost:80");
 
-		if (argc >= 2) host = argv[1];
+		if(argc >= 2) host = argv[1];
 
 		Stopwatch sw;
 		sw.start();
@@ -68,64 +68,16 @@ int main(int argc, char** argv)
 	return 0;
 }
 
-
 #if 0
 
-#include <stdio.h>
-#include <curl/curl.h>
- 
- sinsp_logger g_logger;
- 
+#include "uri.h"
+
 int main(void)
 {
-  CURL *curl;
-  CURLcode res;
- 
-  curl_global_init(CURL_GLOBAL_DEFAULT);
- 
-  curl = curl_easy_init();
-  if(curl) {
-    curl_easy_setopt(curl, CURLOPT_URL, "https://vagrant:vagrant@10.245.1.2/api/v1/nodes");
- 
-//#ifdef SKIP_PEER_VERIFICATION
-    /*
-     * If you want to connect to a site who isn't using a certificate that is
-     * signed by one of the certs in the CA bundle you have, you can skip the
-     * verification of the server's certificate. This makes the connection
-     * A LOT LESS SECURE.
-     *
-     * If you have a CA cert for the server stored someplace else than in the
-     * default bundle, then the CURLOPT_CAPATH option might come handy for
-     * you.
-     */ 
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0L);
-	//curl_easy_setopt(curl, CURLOPT_CAPATH, "/home/alex/sysdig/agent/experiments/kubeget/ca-bundle.crt");
-//#endif
- 
-#ifdef SKIP_HOSTNAME_VERIFICATION
-    /*
-     * If the site you're connecting to uses a different host name that what
-     * they have mentioned in their server certificate's commonName (or
-     * subjectAltName) fields, libcurl will refuse to connect. You can skip
-     * this check, but this will make the connection less secure.
-     */ 
-    curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0L);
-#endif
- 
-    /* Perform the request, res will get the return code */ 
-    res = curl_easy_perform(curl);
-    /* Check for errors */ 
-    if(res != CURLE_OK)
-      fprintf(stderr, "curl_easy_perform() failed: %s\n",
-              curl_easy_strerror(res));
- 
-    /* always cleanup */ 
-    curl_easy_cleanup(curl);
-  }
- 
-  curl_global_cleanup();
- 
-  return 0;
+	std::string s("https://vagrant:vagrant@10.245.1.2:8080/api/v1?param1=one");
+	uri u(s);
+	std::cout << "schema: " << u.get_scheme() << "\nhost: " << u.get_host() << "\nport: " << u.get_port() << "\nusername: " << u.get_user() << "\npassword: " << u.get_password() << "\npath: " << u.get_path() << "\nquery: " << u.get_query() << std::endl;
+	std::cout << u.to_string() << std::endl;
+	return 0;
 }
-
 #endif
