32381063eb2d5792801dd0ea6a9e21fa01e94e1e
Luca Marturana <luca@sysdig.com>
2015-10-27 11:03:00 +0100

    Skip internal kubernetes containers from mounted_fs read

--- userspace/libsanalyzer/analyzer.cpp
+++ userspace/libsanalyzer/analyzer.cpp
@@ -1513,14 +1513,17 @@ void sinsp_analyzer::emit_processes(sinsp_evt* evt, uint64_t sample_duration, bo
 			vector<tuple<string, pid_t, pid_t>> containers_for_mounted_fs;
 			for(auto it = progtable_by_container.begin(); it != progtable_by_container.end(); ++it)
 			{
-				auto long_running_proc = find_if(it->second.begin(), it->second.end(), [this](sinsp_threadinfo* tinfo)
+				if(it->first.find("k8s_POD") != std::string::npos)
 				{
-					return (m_next_flush_time_ns - tinfo->get_main_thread()->m_clone_ts) >= ASSUME_LONG_LIVING_PROCESS_UPTIME_S*ONE_SECOND_IN_NS;
-				});
-				if(long_running_proc != it->second.end())
-				{
-					containers_for_mounted_fs.emplace_back(it->first, (*long_running_proc)->m_pid,
-														   (*long_running_proc)->m_vpid);
+					auto long_running_proc = find_if(it->second.begin(), it->second.end(), [this](sinsp_threadinfo* tinfo)
+					{
+						return (m_next_flush_time_ns - tinfo->get_main_thread()->m_clone_ts) >= ASSUME_LONG_LIVING_PROCESS_UPTIME_S*ONE_SECOND_IN_NS;
+					});
+					if(long_running_proc != it->second.end())
+					{
+						containers_for_mounted_fs.emplace_back(it->first, (*long_running_proc)->m_pid,
+															   (*long_running_proc)->m_vpid);
+					}
 				}
 			}
 			// Add host
