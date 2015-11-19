035f10310e41665edabe4f7d57ca6dd4b0cb7580
ltagliamonte <luigi.tagliamonte86@gmail.com>
2015-11-19 12:59:25 +0100

    support k8s delegate node

--- docker/dev/docker-entrypoint.sh
+++ docker/dev/docker-entrypoint.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 #set -e
 
+#need to test this function on all the supported OS
+function am_i_a_k8s_delegated_node(){
+	my_ip=$(hostname -i)
+	if [ "${K8S_DELEGATED_NODE}" == "${my_ip}" ]; then
+		return 0
+	else
+		return 1
+	fi
+}
+
 echo "* Setting up /usr/src links from host"
 
 for i in $(ls $SYSDIG_HOST_ROOT/usr/src)
@@ -70,15 +80,40 @@ if [ ! -z "$CHECK_CERTIFICATE" ]; then
 	fi
 fi
 
+if [ ! -z "$K8S_DELEGATED_NODE" ]; then
+	if ! am_i_a_k8s_delegated_node; then
+		echo "* Disabling k8s_autodetect"
+		if ! grep ^k8s_autodetect $CONFIG_FILE > /dev/null 2>&1; then
+			echo "k8s_autodetect: false" >> $CONFIG_FILE
+		else
+			sed -i "s/^k8s_autodetect.*/k8s_autodetect: false/g" $CONFIG_FILE
+		fi
+	fi
+fi
+
 if [ ! -z "$ADDITIONAL_CONF" ]; then
 	echo "* Setting additional customer configuration"
-
 	echo -e $ADDITIONAL_CONF | while read line; do
-		if ! grep ^$line $CONFIG_FILE > /dev/null 2>&1; then
-			echo "$line" >> $CONFIG_FILE
+		if [[ $line == *"k8s_"* ]] && [[ ! -z "$K8S_DELEGATED_NODE" ]]; then
+			if am_i_a_k8s_delegated_node; then
+				echo "* Configuring $line"
+				if ! grep ^$line $CONFIG_FILE > /dev/null 2>&1; then
+					echo "$line" >> $CONFIG_FILE
+				else
+					sed -i "s/^$line.*/$line/g" $CONFIG_FILE
+				fi
+			else
+				echo "* Not delegate node skypping k8s configuration"
+				continue
+			fi
 		else
-			sed -i "s/^$line.*/$line/g" $CONFIG_FILE
-		fi 
+			echo "* Configuring $line"
+			if ! grep ^$line $CONFIG_FILE > /dev/null 2>&1; then
+				echo "$line" >> $CONFIG_FILE
+			else
+				sed -i "s/^$line.*/$line/g" $CONFIG_FILE
+			fi
+		fi
 	done
 fi
 
