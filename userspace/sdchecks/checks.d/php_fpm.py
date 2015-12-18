5c5c889f8e9149fd7304e645f43ab5ec5449c2d7
Luca Marturana <luca@sysdig.com>
2015-12-18 16:50:49 +0100

    Refactor php-fpm check to use FastCGI instead of HTTP (so will not require an NGINX to do the check)

--- userspace/sdchecks/checks.d/php_fpm.py
+++ userspace/sdchecks/checks.d/php_fpm.py
@@ -1,10 +1,9 @@
 # 3p
-import requests
+import simplejson as json
 
 # project
 from checks import AgentCheck
-from util import headers
-
+import utils.flup_fcgi_client as fcgi_client 
 
 class PHPFPMCheck(AgentCheck):
     """
@@ -29,8 +28,8 @@ class PHPFPMCheck(AgentCheck):
     }
 
     def check(self, instance):
-        status_url = instance.get('status_url')
-        ping_url = instance.get('ping_url')
+        status_url = instance.get('status_url', '/status')
+        ping_url = instance.get('ping_url', '/ping')
         ping_reply = instance.get('ping_reply')
 
         auth = None
@@ -47,31 +46,59 @@ class PHPFPMCheck(AgentCheck):
 
         pool = None
         status_exception = None
+        host = instance.get("host", "localhost")
+        port = instance.get("port", 9000)
         if status_url is not None:
             try:
-                pool = self._process_status(status_url, auth, tags)
+                pool = self._process_status(status_url, host, port, tags)
             except Exception as e:
                 status_exception = e
                 pass
 
         if ping_url is not None:
-            self._process_ping(ping_url, ping_reply, auth, tags, pool)
+            self._process_ping(ping_url, ping_reply, host, port, tags, pool)
 
         # pylint doesn't understand that we are raising this only if it's here
         if status_exception is not None:
             raise status_exception  # pylint: disable=E0702
 
-    def _process_status(self, status_url, auth, tags):
+    def _request_url(self, url, query, host, port):
+        # Example taken from: http://stackoverflow.com/questions/6801673/python-fastcgi-client
+        fcgi = fcgi_client.FCGIApp(host = host, port = port)
+        env = {
+           'SCRIPT_FILENAME': url,
+           'QUERY_STRING': query,
+           'REQUEST_METHOD': 'GET',
+           'SCRIPT_NAME': url,
+           'REQUEST_URI': url,
+           'GATEWAY_INTERFACE': 'CGI/1.1',
+           'SERVER_SOFTWARE': 'Sysdig Cloud',
+           'REDIRECT_STATUS': '200',
+           'CONTENT_TYPE': '',
+           'CONTENT_LENGTH': '0',
+           'DOCUMENT_URI': url,
+           'DOCUMENT_ROOT': '/var/www/html',
+           #'SERVER_PROTOCOL' : ???
+           'REMOTE_ADDR': '127.0.0.1',
+           'REMOTE_PORT': '123',
+           'SERVER_ADDR': host,
+           'SERVER_PORT': str(port),
+           'SERVER_NAME': host
+           }
+        ret = fcgi(env)
+        self.log.debug("php-fpm returned: %s" % repr(ret))
+        return ret
+
+    def _process_status(self, status_url, host, port, tags):
         data = {}
         try:
             # TODO: adding the 'full' parameter gets you per-process detailed
             # informations, which could be nice to parse and output as metrics
-            resp = requests.get(status_url, auth=auth,
-                                headers=headers(self.agentConfig),
-                                params={'json': True})
-            resp.raise_for_status()
-
-            data = resp.json()
+            code, headers, out, err = self._request_url(status_url, "json=true", host, port)
+            if code.startswith('200'):
+                data = json.loads(out)
+            else:
+                raise Exception("Wrong response code from %s url" % status_url)
         except Exception as e:
             self.log.error("Failed to get metrics from {0}.\nError {1}".format(status_url, e))
             raise
@@ -94,7 +121,7 @@ class PHPFPMCheck(AgentCheck):
         # return pool, to tag the service check with it if we have one
         return pool_name
 
-    def _process_ping(self, ping_url, ping_reply, auth, tags, pool_name):
+    def _process_ping(self, ping_url, ping_reply, host, port, tags, pool_name):
         if ping_reply is None:
             ping_reply = 'pong'
 
@@ -103,13 +130,11 @@ class PHPFPMCheck(AgentCheck):
         try:
             # TODO: adding the 'full' parameter gets you per-process detailed
             # informations, which could be nice to parse and output as metrics
-            resp = requests.get(ping_url, auth=auth,
-                                headers=headers(self.agentConfig))
-            resp.raise_for_status()
-
-            if ping_reply not in resp.text:
-                raise Exception("Received unexpected reply to ping {0}".format(resp.text))
-
+            code, headers, out, err = self._request_url(ping_url, "", host, port)
+            if not code.startswith('200'):
+                raise Exception("Wrong response code from %s url" % ping_url)
+            if ping_reply not in out:
+                raise Exception("Received unexpected reply to ping {0}".format(out))
         except Exception as e:
             self.log.error("Failed to ping FPM pool {0} on URL {1}."
                            "\nError {2}".format(pool_name, ping_url, e))
