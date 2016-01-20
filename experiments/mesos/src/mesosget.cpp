f63eaf82d4f25769a814782087b91981e6af0f35
Alex Fabijanic <alex@sysdig.com>
2016-01-20 15:35:44 -0600

    mesos app/task refactor and fix

--- experiments/mesos/src/mesosget.cpp
+++ experiments/mesos/src/mesosget.cpp
@@ -24,10 +24,17 @@ void print_groups(const ::google::protobuf::RepeatedPtrField< ::draiosproto::mar
 	for(auto group : groups)
 	{
 		std::cout << group.id() << std::endl;
+		for(const auto& app : group.apps())
+		{
+			std::cout << '\t' << app.id() << std::endl;
+			for(const auto& task : app.task_ids())
+			{
+				std::cout << "\t\t" <<  task << std::endl;
+			}
+		}
 		for(auto subgroup : group.groups())
 		{
-			std::cout << subgroup.id() << std::endl;
-			print_groups(subgroup.groups());
+			print_groups(groups);
 		}
 	}
 }
@@ -38,15 +45,15 @@ void print_proto(mesos& m, const std::string& fname)
 	mesos_proto(met).get_proto(m.get_state());
 	//FileOutputStream fos("/home/alex/sysdig/agent/experiments/mesos/" + fname + ".protodump");
 	//fos << met.DebugString();
-	//std::cout << met.DebugString() << std::endl;
+	std::cout << met.DebugString() << std::endl;
 	//std::cout << "++++" << std::endl;
-	print_groups(met.mesos().groups());
+	//print_groups(met.mesos().groups());
 	//std::cout << "----" << std::endl;
 }
 
 int main(int argc, char** argv)
 {
-	std::string ip_addr = "52.23.156.41";
+	std::string ip_addr = "52.90.231.127";
 	std::vector<std::string> marathon_uris;
 	marathon_uris.push_back("http://" + ip_addr + ":8080");
 	mesos m("http://" + ip_addr + ":5050", "/master/state", 
@@ -58,11 +65,13 @@ int main(int argc, char** argv)
 	//print_proto(m, ip_addr);
 
 	//m.refresh(true);
+	//print_proto(m, ip_addr);
 	while(true)
 	{
 		//print_proto(m, ip_addr);
+		m.refresh(false);
 		m.watch();
-		sleep(1);
+		sleep(5);
 	}
 
 	return 0;
