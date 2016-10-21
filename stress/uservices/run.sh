e7bb76604916cfb02d0c25e3233fb280ba0cda4d
Loris Degioanni <loris@sysdig.com>
2016-10-21 06:11:09 -0700

    uservices demo improvement

--- stress/uservices/run.sh
+++ stress/uservices/run.sh
@@ -18,18 +18,18 @@ function start() {
 #	docker run -d --name=s_movies --link srvc_node2:srvc_next0 --link srvc_node5:srvc_next1 -e ROLE=root -e NAME=s_movies -e NC=2 -e CPU_OPS=20000 us
 #	docker run -d --name=s_users --link srvc_node2:srvc_next0 --link srvc_node6:srvc_next1 -e ROLE=root -e NAME=s_users -e NC=2 -e CPU_OPS=40000 us
 
-	docker run -d --name=cnt_db -e ROLE=node -e NAME=s_db -e IO_OPS=100000 -e NC=0 us
-	docker run -d --name=cnt_cache -e ROLE=node -e NAME=s_cache -e IO_OPS=20000 -e NC=0 us
+	docker run -d --name=db -e ROLE=node -e NAME=s_db -e IO_OPS=100000 -e NC=0 us
+	docker run -d --name=cache -e ROLE=node -e NAME=s_cache -e IO_OPS=20000 -e NC=0 us
 
-	docker run -d --name=cnt_moviequeue -e CHILD_NAMES='[{"e":["dbquery"]}]' --link cnt_db:srvc_next0 -e ROLE=node -e NAME=s_moviequeue -e NC=1 us
+	docker run -d --name=moviequeue -e CHILD_NAMES='[{"e":["dbquery"]}]' --link db:srvc_next0 -e ROLE=node -e NAME=s_moviequeue -e NC=1 us
 
 
-	docker run -d --name=cnt_usermanager -e CHILD_NAMES='[{"e":["validate","getinfo","query"]}]' --link cnt_db:srvc_next0 -e ROLE=node -e NAME=s_usermanager -e NC=1 us
-	docker run -d --name=cnt_moviemanager -e CHILD_NAMES='[{"e":["store","info","query"]}]' --link cnt_moviequeue:srvc_next0 -e ROLE=node -e NAME=s_moviemanager -e NC=1 us
+	docker run -d --name=usermanager -e CHILD_NAMES='[{"e":["validate","getinfo","query"]}]' --link db:srvc_next0 -e ROLE=node -e NAME=s_usermanager -e NC=1 us
+	docker run -d --name=moviemanager -e CHILD_NAMES='[{"e":["store","info","query"]}]' --link moviequeue:srvc_next0 -e ROLE=node -e NAME=s_moviemanager -e NC=1 us
 
-	docker run -d --name=cnt_users --link cnt_usermanager:srvc_next0 --link cnt_cache:srvc_next1 -e CHILD_NAMES='[{"e":["login","stats","add","delete"]}, {"e":["user_cache"]}]' -e ROLE=root -e NAME=s_users -e NC=2 us
+	docker run -d --name=users --link usermanager:srvc_next0 --link cache:srvc_next1 -e CHILD_NAMES='[{"e":["login","stats","add","delete"]}, {"e":["user_cache"]}]' -e ROLE=root -e NAME=s_users -e NC=2 us
 
-	docker run -d --name=cnt_movies --link cnt_moviemanager:srvc_next0 --link cnt_cache:srvc_next1 -e CHILD_NAMES='[{"e":["list","add","delete","rate"]}, {"e":["movie_cache"]}]' -e ROLE=root -e NAME=s_movies -e NC=2 us
+	docker run -d --name=movies --link moviemanager:srvc_next0 --link cache:srvc_next1 -e CHILD_NAMES='[{"e":["list","add","delete","rate"]}, {"e":["movie_cache"]}]' -e ROLE=root -e NAME=s_movies -e NC=2 us
 
 
 #	docker run -d --name=srvc_node6 -e ROLE=node -e NAME=srvc_node6 -e NC=0 us
