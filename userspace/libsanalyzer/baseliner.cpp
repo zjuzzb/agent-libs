34628c4ab5b697855bb79806a35262da07b368ec
Loris Degioanni <loris@sysdig.com>
2017-01-01 23:00:22 +0000

    initial implementation of the memory dumper

--- userspace/libsanalyzer/baseliner.cpp
+++ userspace/libsanalyzer/baseliner.cpp
@@ -247,6 +247,7 @@ void sisnp_baseliner::register_callbacks(sinsp_fd_listener* listener)
 	//
 	// Initialize the FD listener
 	//
+// xxxx
 	m_inspector->m_parser->m_fd_listener = listener;
 }
 
@@ -885,3 +886,151 @@ void sisnp_baseliner::process_event(sinsp_evt *evt)
 	return evname;
 }
 #endif
+
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////
+sinsp_memory_dumper::sinsp_memory_dumper(sinsp* inspector)
+{
+	m_inspector = inspector;
+	m_active_state = &m_states[0];
+	m_file_id = 0;
+	m_f = NULL;
+}
+
+void sinsp_memory_dumper::init(uint64_t bufsize)
+{
+	uint64_t bsize = bufsize / 2;
+
+	m_states[0].init(m_inspector, bsize);
+	m_states[1].init(m_inspector, bsize);
+
+	m_inspector->m_is_dumping = true;
+
+	try
+	{
+		m_active_state->m_dumper->open("", false, true);
+		m_active_state->m_has_data = true;
+	}
+	catch(sinsp_exception e)
+	{
+		throw sinsp_exception(
+			"capture memory buffer too small to store process information. Current size: " + 
+			to_string(bsize));
+	}
+
+
+	char tbuf[32768];
+	struct timeval ts;
+	gettimeofday(&ts, NULL);
+	time_t rawtime = (time_t)ts.tv_sec;
+	struct tm* time_info = gmtime(&rawtime);
+	snprintf(tbuf, sizeof(tbuf), "%.2d-%.2d_%.2d_%.2d_%.2d_%.6d",
+		time_info->tm_mon + 1,
+		time_info->tm_mday,
+		time_info->tm_hour,
+		time_info->tm_min,
+		time_info->tm_sec,
+		(int)ts.tv_usec);
+
+	string fname = string("sd_dump_") + tbuf + ".scap";
+
+	m_f = fopen(fname.c_str(), "wb");
+	if(m_f == NULL)
+	{
+		lo("cannot open file %s", fname.c_str());
+	}
+}
+
+sinsp_memory_dumper::~sinsp_memory_dumper()
+{
+	if(m_f != NULL)
+	{
+		fclose(m_f);
+	}
+}
+
+void sinsp_memory_dumper::close()
+{
+	switch_states();
+	m_inspector->m_is_dumping = false;
+}
+
+void sinsp_memory_dumper::flush_state_to_disk(sinsp_memory_dumper_state* state)
+{
+	if(state->m_has_data)
+	{
+		uint64_t datalen1 = state->m_dumper->written_bytes();
+		uint64_t datalen = state->m_last_valid_bufpos - state->m_buf;
+		m_file_id++;
+
+		fwrite(state->m_buf, datalen, 1, m_f);
+
+		/*
+		uint64_t dlen = 400 * 1024 * 1024;
+		printf("YYY\n");
+
+		if(compr(state->m_buf + datalen + 1,
+			&dlen, state->m_buf, datalen, 1) != SCAP_SUCCESS)
+		{
+			printf("FAILED\n");
+		}
+		printf("XXX %ld %ld\n", datalen, dlen);
+
+		fwrite(state->m_buf + datalen + 1, dlen, 1, f);
+		*/
+	}
+}
+
+void sinsp_memory_dumper::switch_states()
+{
+	//
+	// The buffer is full, swap the states
+	//
+	if(m_active_state == &m_states[0])
+	{
+		m_active_state = &m_states[1];
+	}
+	else
+	{
+		m_active_state = &m_states[0];
+	}
+
+	//
+	// Save the capture to disk
+	//
+	flush_state_to_disk(m_active_state);
+
+	//
+	// Close and reopen the new state
+	//
+	m_active_state->m_dumper->close();
+
+	try
+	{
+		m_active_state->m_dumper->open("", false, true);
+		m_active_state->m_has_data = true;
+	}
+	catch(sinsp_exception e)
+	{
+		throw sinsp_exception(
+			"capture memory buffer too small to store process information. Current size: " + 
+			m_active_state->m_bufsize);
+	}
+}
+
+void sinsp_memory_dumper::process_event(sinsp_evt *evt)
+{
+	try
+	{
+		m_active_state->m_last_valid_bufpos = m_active_state->m_dumper->get_memory_dump_cur_buf();
+		m_active_state->m_dumper->dump(evt);
+	}
+	catch(sinsp_exception e)
+	{
+		switch_states();
+		m_active_state->m_dumper->dump(evt);
+	}
+}
