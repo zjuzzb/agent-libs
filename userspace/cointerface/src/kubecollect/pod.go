48c3bd501dc1d659eebde7e7f5f6a0f83f262c1b
Brett <brett@sysdig.com>
2017-07-14 15:31:37 -0700

    add container children to pods

--- userspace/cointerface/src/kubecollect/pod.go
+++ userspace/cointerface/src/kubecollect/pod.go
@@ -37,17 +37,27 @@ func newPodCongroup(pod *v1.Pod) (*draiosproto.ContainerGroup) {
 		ips = append(ips, pod.Status.PodIP)
 	}
 
+	var cids []*draiosproto.CongroupUid
+	for _, c := range pod.Status.ContainerStatuses {
+		cids = append(cids, &draiosproto.CongroupUid{
+			Kind:proto.String("container"),
+			Id:proto.String(c.ContainerID)})
+	}
+
 	return &draiosproto.ContainerGroup{
 		Uid: &draiosproto.CongroupUid{
 			Kind:proto.String("k8s_pod"),
 			Id:proto.String(string(pod.GetUID()))},
 		Tags: tags,
 		IpAddresses: ips,
+		Children: cids,
 		Parents: nil,
 	}
 }
 
 func WatchPods(ctx context.Context, kubeClient kubeclient.Interface, evtc chan<- draiosproto.CongroupUpdateEvent) {
+	log.Debugf("In WatchPods()")
+
 	client := kubeClient.CoreV1().RESTClient()
 	lw := cache.NewListWatchFromClient(client, "pods", v1meta.NamespaceAll, nil)
 	resyncPeriod := time.Duration(10) * time.Second;
@@ -56,7 +66,7 @@ func WatchPods(ctx context.Context, kubeClient kubeclient.Interface, evtc chan<-
 	inf.AddEventHandler(
 		cache.ResourceEventHandlerFuncs{
 			AddFunc: func(obj interface{}) {
-				log.Debugf("AddFunc dumping pod: %v", obj.(*v1.Pod))
+				//log.Debugf("AddFunc dumping pod: %v", obj.(*v1.Pod))
 				evtc <- podEvent(obj.(*v1.Pod),
 					draiosproto.CongroupEventType_ADDED.Enum())
 			},
@@ -64,14 +74,14 @@ func WatchPods(ctx context.Context, kubeClient kubeclient.Interface, evtc chan<-
 				oldPod := oldObj.(*v1.Pod)
 				newPod := newObj.(*v1.Pod)
 				if oldPod.GetResourceVersion() != newPod.GetResourceVersion() {
-					log.Debugf("UpdateFunc dumping pod oldPod %v", oldPod)
-					log.Debugf("UpdateFunc dumping pod newPod %v", newPod)
+					//log.Debugf("UpdateFunc dumping pod oldPod %v", oldPod)
+					//log.Debugf("UpdateFunc dumping pod newPod %v", newPod)
 					evtc <- podEvent(newPod,
 						draiosproto.CongroupEventType_UPDATED.Enum())
 				}
 			},
 			DeleteFunc: func(obj interface{}) {
-				log.Debugf("DeleteFunc dumping pod: %v", obj.(*v1.Pod))
+				//log.Debugf("DeleteFunc dumping pod: %v", obj.(*v1.Pod))
 				evtc <- podEvent(obj.(*v1.Pod),
 					draiosproto.CongroupEventType_REMOVED.Enum())
 			},
