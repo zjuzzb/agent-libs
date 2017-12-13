e302bcb51deedf7e090a3749a8d727a2cf8fde8c
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2017-12-13 22:14:04 +0100

    Allow open->openat rewrite. SMAGENT-744
    Recent Ubuntu releases (at least 17.10) use the openat() syscall
    for libc open() calls. Adapt the test code to cope with it.
    
    This commit fixes the memdump tests to use a filter that will
    catch both open< and openat> events (the name in openat is reported
    in the enter event, unlike open).

--- test/memdump.cpp
+++ test/memdump.cpp
@@ -444,7 +444,7 @@ protected:
 		std::unique_ptr<sinsp> inspector = make_unique<sinsp>();
 		set<string> found;
 		sinsp_evt_formatter open_name(inspector.get(), "%evt.arg.name");
-		string filter = string("evt.type=open and evt.dir=< and evt.is_open_read=true and fd.name startswith ")
+		string filter = string("evt.is_open_read=true and evt.arg.name startswith ")
 			+ memdump_test::test_filename_pat;
 
 		g_log->debug("Searching through trace file with tag=" + tag + " with filter " + filter);
