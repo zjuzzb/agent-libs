8fdbf7b7afe9e7bfeabd3e6bec4017c9f1b3b552
anoop-sysd <31664950+anoop-sysd@users.noreply.github.com>
2018-06-06 16:33:50 -0700

    SMAGENT-835: Remove Java version checking (#862)
    * SMAGENT-835: Remove Java version checking
    
    * SMAGENT-835: Fix comments, error message

--- userspace/sdjagent/src/main/java/com/sysdigcloud/sdjagent/ApplicationLoader.java
+++ userspace/sdjagent/src/main/java/com/sysdigcloud/sdjagent/ApplicationLoader.java
@@ -7,40 +7,24 @@
 package com.sysdigcloud.sdjagent;
 
 /**
- * This class has been created to avoid crashes on java < 7.
  * Since we have some deps that are compiled for JVM 7, loading Application.class
- * fails without us have any control on it. This class has a bare minimum set of dependencies,
- * it's able to catch UnsupportedClassVersionError and exit nicely
+ * fails without us have any control on it if JVM version is < 7.
+ * This class has a bare minimum set of dependencies, it's able to catch
+ * UnsupportedClassVersionError and exit nicely
  */
 public class ApplicationLoader {
     public static final double MIN_JAVA_VERSION = 1.7;
 
     public static void main(String[] args) {
-        if (getJavaVersion() < MIN_JAVA_VERSION) {
-            String msg = "Java 7 or later must be used. Current version is " + System.getProperty("java.version");
-            System.err.println("{\"pid\": 0, \"level\": \"SEVERE\", \"message\": \"" + msg + "\"}");
-            System.exit(Application.MONITOR_DONT_RESTART_CODE);
-        }
-
         try {
             Application.main(args);
         }
         catch (java.lang.UnsupportedClassVersionError exc)
         {
-            String msg = "Incompatible class loaded: " + exc.getMessage();
+            String msg = "Incompatible class loaded: " + exc.getMessage() +
+                         " (Java version: " + System.getProperty("java.version") + ", must be >=" + MIN_JAVA_VERSION + ")";
             System.err.println("{\"pid\": 0, \"level\": \"SEVERE\", \"message\": \"" + msg + "\"}");
             System.exit(Application.MONITOR_DONT_RESTART_CODE);
         }
-    }
-
-    public static double getJavaVersion() {
-        String version = System.getProperty("java.version");
-        int pos = version.indexOf('.');
-        pos = version.indexOf('.', pos+1);
-        int dash_pos = version.indexOf('-');
-        if (dash_pos != -1 && (pos == -1 || pos > dash_pos)) {
-            pos = dash_pos;
-        }
-        return Double.parseDouble(version.substring (0, pos));
     }
 }
