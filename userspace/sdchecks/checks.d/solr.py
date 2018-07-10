3056f81047ee811b3fbcc282a7702111e2e6fcab
anoop-sysd <31664950+anoop-sysd@users.noreply.github.com>
2018-07-10 13:03:32 -0700

    SMAGENT-976: add error message if query in getUrl() times out. (#923)
    * SMAGENT-976: add error message if query in getUrl() times out.
    add a common prefix to all log messages.
    
    * SMAGENT-976: fix log levels

--- userspace/sdchecks/checks.d/solr.py
+++ userspace/sdchecks/checks.d/solr.py
@@ -123,19 +123,21 @@ class Solr(AgentCheck):
 
     def getUrl(self, url):
         if url in self.cache:
-            self.log.debug("Solr, getting url {} from cache".format(url))
+            self.log.debug("solr: getting url response from cache: {}".format(url))
             return self.cache.get(url)
 
-        self.log.debug("Solr, getting url: {}".format(url))
+        self.log.debug("solr: querying url: {}".format(url))
         try:
             data = urllib2.urlopen(url, None, self.timeout)
             obj = json.load(data)
-        except:
+        except Exception as e:
+            self.log.debug(str("solr: error while querying url {}: {}").format(url, e))
             # Cache failures as well, so we don't retry
             self.cache[url] = {}
             return {}
 
-        if obj is None:
+        if obj is None or len(obj) == 0:
+            self.log.debug(str("solr: empty response from url {}").format(url))
             self.cache[url] = {}
             return {}
 
@@ -145,7 +147,7 @@ class Solr(AgentCheck):
     def scanPorts(self, handler):
         ports = [ self.port ] if self.port else self.ports
 
-        self.log.debug("Solr.scanPorts: host {} ports {} handler {}".format(self.host, self.ports, handler))
+        self.log.debug("solr: scanPorts: host {} ports {} handler {}".format(self.host, self.ports, handler))
         for port in ports:
             url = "http://" + self.host + ":" + str(port) + handler
             obj = self.getUrl(url)
@@ -166,9 +168,9 @@ class Solr(AgentCheck):
     def _isLocal(self, ip, port):
         try:
             ret = self.network.ipIsLocalHostOrDockerContainer(ip) and int(self.port) == int(port)
-            self.log.debug("{}:{} is {} to {}:{}".format(ip, port, "local" if ret else "not local", "localhost", self.port))
+            self.log.debug("solr: {}:{} is {} to {}:{}".format(ip, port, "local" if ret else "not local", "localhost", self.port))
         except:
-            self.log.warning("Failed to determine locality of {}:{} on {}:{}".format(ip, port, "localhost", self.port))
+            self.log.warning("solr: Failed to determine if {}:{} is local to {}:{}".format(ip, port, "localhost", self.port))
             return False
 
         return ret
@@ -187,7 +189,7 @@ class Solr(AgentCheck):
                         live_node_count += 1
 
                 self.gauge(self.METRIC_LIVE_NODES, live_node_count, confTags)
-                self.log.debug(("detected {} live local nodes").format(live_node_count))
+                self.log.debug(("solr: detected {} live local nodes").format(live_node_count))
             except KeyError:
                 pass
 
@@ -202,7 +204,7 @@ class Solr(AgentCheck):
                 tags = confTags + [ self.TAG_COLLECTION % collection ]
                 self.gauge(self.METRIC_COLLECTION_SHARD_COUNT, shards_per_collection, tags)
         except Exception as e:
-            self.log.error(("Error while fetching collection shard count: {}").format(e))
+            self.log.error(("solr: Error while fetching collection shard count: {}").format(e))
 
     def _getHostShardCount(self, confTags):
         try:
@@ -229,7 +231,7 @@ class Solr(AgentCheck):
                 tags = confTags + [ self.TAG_COLLECTION % collection ]
                 self.gauge(self.METRIC_HOST_SHARD_COUNT, shards_per_host, tags)
         except Exception as e:
-            self.log.error(("Error while fetching host shard count: {}").format(e))
+            self.log.error(("solr: Error while fetching host shard count: {}").format(e))
 
     def _getReplica(self, confTags):
         try:
@@ -249,9 +251,9 @@ class Solr(AgentCheck):
                     if replicaCount > 0:
                         tags = confTags + [ self.TAG_COLLECTION % collectionName, ]
                         self.gauge(self.METRIC_REPLICA, replicaCount, tags)
-                        self.log.debug(("detected {} replica with tags {}").format(replicaCount, tags))
+                        self.log.debug(("solr: detected {} replicas with tags {}").format(replicaCount, tags))
         except Exception as e:
-            self.log.error(("Error while fetching replica: {}").format(e))
+            self.log.error(("solr: Error while fetching replicas: {}").format(e))
 
     def _getLocalDocumentCount(self, confTags):
         for base in self.localEndpoints:
@@ -267,6 +269,8 @@ class Solr(AgentCheck):
                     if collectionName is not None:
                         tags.append(self.TAG_COLLECTION % collectionName)
 
+                    self.log.debug(str("solr: checking document count for core {}").format(core_name))
+
                     if core_name not in self.localLeaderCores:
                         # Report 0 for non-leader cores so that the number panel in the host specific dashboard has data to show
                         self.gauge(self.METRIC_DOCUMENT_COUNT, 0, tags)
@@ -274,6 +278,7 @@ class Solr(AgentCheck):
                         self.gauge(self.METRIC_DOCUMENT_COUNT_DELETED, 0, tags)
                         continue
 
+                    self.log.debug(str("solr: fetching document count for local leader core {}").format(core_name))
                     numDocs = obj["status"][core_name]["index"]["numDocs"]
                     maxDoc = obj["status"][core_name]["index"]["maxDoc"]
                     deletedDocs = obj["status"][core_name]["index"]["deletedDocs"]
@@ -282,7 +287,7 @@ class Solr(AgentCheck):
                     self.gauge(self.METRIC_DOCUMENT_COUNT_MAX, maxDoc, tags)
                     self.gauge(self.METRIC_DOCUMENT_COUNT_DELETED, deletedDocs, tags)
         except Exception as e:
-            self.log.error(("Error while fetching core document count: {}").format(e))
+            self.log.error(("solr: Error while fetching document count from {}: {}").format(base, e))
 
     def _retrieveLocalEndpointsAndCores(self):
         obj = self.getHandler(self.ENDPOINT_LIVE_NODES)
@@ -292,7 +297,7 @@ class Solr(AgentCheck):
                     for shardName, shard in collection["shards"].iteritems():
                         for coreAlias, replica in shard["replicas"].iteritems():
                             if replica["state"] != "active":
-                                self.log.debug(("Skipping core {}_{}_{} in state {} on node {}").format(collectionName, shardName, coreAlias, replica["state"], replica["base_url"]))
+                                self.log.debug(("solr: Skipping core {}_{}_{} in state {} on node {}").format(collectionName, shardName, coreAlias, replica["state"], replica["base_url"]))
                                 continue
                             base_url = replica["base_url"]
                             parsedUrl = urlparse(base_url)
@@ -311,15 +316,15 @@ class Solr(AgentCheck):
                                 # Since we're matching both ip and port
                                 # localEndpoints should only have one base_url
                                 self.localEndpoints.add(base_url)
-                                self.log.debug(str("detected local core {}:{} on node {}").format(coreName, coreAlias, base_url))
+                                self.log.debug(str("solr: detected local core {}:{} on node {}").format(coreName, coreAlias, base_url))
             except Exception as e:
-                self.log.error(("Error while attempting to fetch local cores: {}").format(e))
+                self.log.error(("solr: Error while attempting to fetch local cores: {}").format(e))
 
     def _getAllRpsAndRequestTime(self, confTags):
         coresStatistic = self._getStats()
-        self.log.debug(str("fetching statistics for {} local cores").format(len(coresStatistic)))
+        self.log.debug(str("solr: fetching rps+rt metrics for {} local cores").format(len(coresStatistic)))
         for coreStat in coresStatistic:
-            self.log.debug(str("fetching statistics for local core: {}.{}").format(coreStat.core.name, coreStat.core.alias))
+            self.log.debug(str("solr: fetching rps+rt metrics for local core: {}.{}").format(coreStat.core.name, coreStat.core.alias))
             # create tags here
             collection = coreStat.core.collection
             coreName = coreStat.core.name
@@ -373,20 +378,20 @@ class Solr(AgentCheck):
             self._getSingleRequestTime(self.METRIC_QUERY_RT, "/query", queryHandlerObj, tags)
             self._getSingleRequestTime(self.METRIC_UPDATE_RT, "/update", queryHandlerObj, tags)
         except Exception as e:
-            self.log.debug(("could not get statistic from local core: {}").format(e))
+            self.log.debug(("solr: unable to get rps+rt metrics for local core: {}").format(e))
 
     def _getSingleRps(self, metricName, keyString, queryHandlerObj, tags):
         try:
             self.rate(metricName, queryHandlerObj[keyString]["stats"]["requests"], tags)
         except Exception as e:
-            self.log.debug(("could not get rps {} {}: {}").format(metricName, keyString, e))
+            self.log.debug(("solr: could not get rps {} {}: {}").format(metricName, keyString, e))
 
     def _getSingleRequestTime(self, metricName, keyString, queryHandlerObj, tags):
         try:
             reqs = int(queryHandlerObj[keyString]["stats"]["requests"])
             tottime = float(queryHandlerObj[keyString]["stats"]["totalTime"])
         except Exception as e:
-            self.log.debug(("could not get request time {} {}: {}").format(metricName, keyString, e))
+            self.log.debug(("solr: could not get request time {} {}: {}").format(metricName, keyString, e))
 
         key = tuple(tags) + (("type:%s" % keyString), )
         prevStats = self.prevStats.get(key, None)
@@ -396,11 +401,11 @@ class Solr(AgentCheck):
             if dreqs > 0 and dtime > 0:
                 self.gauge(metricName, float(dtime / dreqs), tags)
             elif dreqs < 0 or dtime < 0:
-                self.log.debug("decreased request count or total time, resetting stored stats for {}".format(key))
+                self.log.debug("solr: decreased request count or total time, resetting stored stats for {}".format(key))
             elif dreqs != 0 or dtime != 0:
                 self.log.info("solr: request time/count inconsistent: {}/{} for {}".format(dtime, dreqs, key))
         else:
-            self.log.debug("Previous req stats not found for {}".format(key))
+            self.log.debug("solr: Previous req stats not found for {}".format(key))
 
         self.prevStats[key] = self.PrevStat(reqs, tottime)
 
@@ -416,9 +421,15 @@ class Solr(AgentCheck):
             self.rate(self.METRIC_UPDATEHANDLER_COMMITS, long(stats["updateHandler"]["stats"]["commits"]), tags)
             self.rate(self.METRIC_UPDATEHANDLER_AUTOCOMMITS, long(stats["updateHandler"]["stats"]["autocommits"]), tags)
         except Exception as e:
-            self.log.debug(("unable to get updatehandler stats: {}").format(e))
+            self.log.debug(("solr: unable to get updatehandler stats: {}").format(e))
 
+    # It is assumed that this implementation must always find index size.
+    # Failure to find the metric is logged as an error.
     def _getFromCoreIndexSize(self, coreStatistic, confTags):
+        if coreStatistic.data is None or len(coreStatistic.data) == 0:
+            self.log.debug(str("solr: Unable to get index size from empty stats for core: {}").format(coreStatistic.core.name))
+            return
+
         tags = confTags + [
             self.TAG_COLLECTION % coreStatistic.core.collection,
             self.TAG_CORE % coreStatistic.core.name,
@@ -428,14 +439,14 @@ class Solr(AgentCheck):
         try:
             beans = coreStatistic.data["solr-mbeans"]
             if beans[0] != "CORE":
-                raise Exception('CORE not in position 0 in solr-mbeans')
+                raise Exception('solr: CORE not in position 0 in solr-mbeans')
             if beans[2] != "QUERYHANDLER":
-                raise Exception('QUERYHANDLER not in position 2 in solr-mbeans')
+                raise Exception('solr: QUERYHANDLER not in position 2 in solr-mbeans')
 
             handlers = beans[3]
             if '/replication' in handlers:
                 indexSize = handlers["/replication"]["stats"]["indexSize"]
-                self.log.debug(str("index size retrieved from /replication for core {}: {}").format(coreStatistic.core.name, indexSize))
+                self.log.debug(str("solr: index size retrieved from /replication for core {}: {}").format(coreStatistic.core.name, indexSize))
 
                 val, unit = indexSize.split()
 
@@ -450,7 +461,7 @@ class Solr(AgentCheck):
                     valInBytes = long(val)
             else:
                 indexSize = beans[1]["core"]["stats"]["sizeInBytes"]
-                self.log.debug(str("index size retrieved from core stats for core {}: {}").format(coreStatistic.core.name, indexSize))
+                self.log.debug(str("solr: index size retrieved from core stats for core {}: {}").format(coreStatistic.core.name, indexSize))
                 valInBytes = long(indexSize)
 
             self.gauge(self.METRIC_INDEX_SIZE_REP, valInBytes, tags)
@@ -459,7 +470,7 @@ class Solr(AgentCheck):
             logicalSize = valInBytes if coreStatistic.core.name in self.localLeaderCores else 0
             self.gauge(self.METRIC_INDEX_SIZE_LOG, logicalSize, tags)
         except Exception as e:
-            self.log.error(("Error getting index size for core {}: {}").format(coreStatistic.core.name, e))
+            self.log.error(("solr: Error getting index size for core {}: {}").format(coreStatistic.core.name, e))
 
     def check(self, instance):
         # Don't waste time retrying if we failed because of bad version
@@ -476,21 +487,20 @@ class Solr(AgentCheck):
             if self.version is None:
                 # Default retry behavior is managed by config and sdchecks.py
                 # We might have more luck next time?
-                raise CheckException("Failed to find Solr version")
+                raise CheckException("solr: Failed to determine version")
             elif self.version[0:1] != "5":
                 # Don't retry, cause we would always end up here.
                 self.failed = True
-                raise CheckException("Solr version {} not yet supported".format(self.version[0:1]))
+                raise CheckException("solr: Version {} not yet supported".format(self.version[0:1]))
 
         confTags = instance.get('tags', [])
-
         self.SolrMetriccheck(confTags)
 
     def _getSolrVersion(self, instance):
         if self.version == None:
-            self.log.debug("Solr, getting version from ports {}, endpoint {}".format(instance["ports"], self.ENDPOINT_VERSION))
+            self.log.debug("solr: getting version from ports {}, endpoint {}".format(instance["ports"], self.ENDPOINT_VERSION))
             obj = self.scanPorts(self.ENDPOINT_VERSION)
-            self.log.debug("Solr, version check (port {}) returns: {}".format(self.port, obj))
+            self.log.debug("solr: version check at port {} returned {}".format(self.port, obj))
             if len(obj) > 0:
                 self.log.debug(str("solr: version endpoint found on port {} out of ports {}").format(self.port, instance["ports"]))
                 self.version = obj["lucene"]["solr-spec-version"]
