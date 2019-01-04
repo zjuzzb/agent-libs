4dab198dda0f47492fb46afa3a1858774b6a1329
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2019-01-04 16:29:58 +0100

    Pass the Python binary path as sdchecks' argv[0]
    Python uses its argv[0] to find its files. This is particularly
    important if the Python install has been moved from its
    configured location. Then, Python cannot use the prefix embedded
    in the binary, and cannot detect it from argv[0], so it needs
    $PYTHONHOME set. This is an extra setup step that could be avoided.

--- userspace/dragent/dragent.cpp
+++ userspace/dragent/dragent.cpp
@@ -484,7 +484,8 @@ int dragent_app::main(const std::vector<std::string>& args)
 			this->m_sdchecks_pipes->attach_child();
 
 			setenv("LD_LIBRARY_PATH", (m_configuration.m_root_dir + "/lib").c_str(), 1);
-			execl(this->m_configuration.m_python_binary.c_str(), "python", (m_configuration.m_root_dir + "/bin/sdchecks").c_str(), NULL);
+			const char *python = this->m_configuration.m_python_binary.c_str();
+			execl(python, python, (m_configuration.m_root_dir + "/bin/sdchecks").c_str(), NULL);
 
 			return (EXIT_FAILURE);
 		});
