084fad4cd275f7da289a907913a418dee668dc9c
Bryan Seay <42394778+bryanseay@users.noreply.github.com>
2019-01-28 19:43:02 -0700

    fix up to work across files (#1185)

--- userspace/cointerface/src/test_helpers/assert.go
+++ userspace/cointerface/src/test_helpers/assert.go
@@ -2,23 +2,17 @@ package test_helpers
 
 import (
 	"fmt"
+	"path/filepath"
 	"reflect"
 	"runtime"
 	"testing"
 )
 
 // Go up a level in the stack frame so that we print the write file/line
-func up(frame int, s string) string {
-    _, origFile, origLine, _ := runtime.Caller(1)
+func up(t *testing.T, frame int, s string) string {
     _, frameFile, frameLine, _ := runtime.Caller(frame + 1)
-    if origFile != frameFile {
-        return s // Deferred call after a panic or runtime.Goexit()
-    }
-    erase := []byte("\b\b\b")
-    for ; origLine > 9; origLine /= 10 {
-        erase = append(erase, '\b')
-    }
-    return fmt.Sprintf("%s%d: %s", erase, frameLine, s)
+	_, file := filepath.Split(frameFile)
+    return fmt.Sprintf("%s:%d %s", file, frameLine, s)
 }
 
 // AssertEqual checks if values are equal
@@ -27,6 +21,6 @@ func AssertEqual(t *testing.T, expected interface{}, actual interface{}) {
 		return
 	}
 	error := fmt.Sprintf("ASSERTION FAILED\nReceived [%v]: %v \nExpected [%v]: %v ", reflect.TypeOf(actual), actual, reflect.TypeOf(expected), expected)
-	t.Errorf(up(1, error))
+	t.Errorf("\n" + up(t, 1, error))
 	t.Fail()
 }
