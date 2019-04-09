c06902c3763307fb48b845ff7ea20eca9f6ad37c
Nathan Baker <7409217+nathan-b@users.noreply.github.com>
2019-04-09 14:52:31 -0400

    [SMAGENT-1460]: Added connection manager unit test (#1302)
    * [SMAGENT-1460]: Added connection manager unit test
    
    - Built fake collector
    - Wrote simple connection and transmit unit test for connection manager
      using the fake collector library
    
    * Fixed dumb typo in the cleanup code...
    
    * Removed rogue printf
    
    * Don't need executable and library
    
    * Addressed review comments
    
    * Addressed more review comments

---
+++ userspace/dragent/test/connection_manager.ut.cpp
@@ -0,0 +1,132 @@
+using namespace std;
+#include <gtest.h>
+#include <chrono>
+#include <thread>
+#include <list>
+#include <sstream>
+
+#include <unistd.h>
+#include <ctime>
+
+#include "configuration.h"
+#include "sinsp_worker.h"
+#include "sinsp_mock.h"
+#include "watchdog_runnable.h"
+#include "connection_manager.h"
+#include "fake_collector.h"
+
+using namespace dragent;
+using namespace test_helpers;
+
+namespace
+{
+/**
+ * Sleep for the given number of milliseconds
+ *
+ * @param milliseconds  Number of ms to sleep for
+ */
+void msleep(uint32_t milliseconds)
+{
+    struct timespec ts =
+    {
+    	.tv_sec = milliseconds / 1000,
+    	.tv_nsec = (milliseconds % 1000) * 1000000,
+    };
+    nanosleep(&ts, NULL);
+}
+
+/**
+ * Builds a randomized string of the specified length.
+ *
+ * @param len  The length of the string to build (not including null terminator)
+ *
+ * @return The requested string
+ */
+std::string build_test_string(uint32_t len)
+{
+	static const std::string alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	std::stringstream ss;
+
+	for(uint32_t i = 0; i < len; ++i)
+	{
+		ss << alpha[rand() % alpha.size()];
+	}
+
+	return ss.str();
+}
+} // End namespace
+
+TEST(connection_manager_test, connect_transmit)
+{
+	const size_t MAX_QUEUE_LEN = 64;
+	// Build some boilerplate stuff that's needed to build a CM object
+	dragent_configuration::m_terminate = false;
+	dragent_configuration config;
+	config.init();
+
+	// Create and spin up the fake collector (get an ephemeral port)
+	fake_collector fc;
+	fc.start(0);
+
+	ASSERT_GT(fc.get_port(), 0);
+
+	// Set the config for the CM
+	config.m_server_addr = "127.0.0.1";
+	config.m_server_port = fc.get_port();
+	config.m_ssl_enabled = false;
+	config.m_transmitbuffer_size = DEFAULT_DATA_SOCKET_BUF_SIZE;
+	config.m_terminate = false;
+
+	// Create the shared blocking queue
+	protocol_queue queue(MAX_QUEUE_LEN);
+
+
+	// Create and spin up the connection manager
+	std::thread t([&queue, &config]()
+	{
+		// The sinsp_worker and c_j_h are only used when processing
+		// push messages from the backend. For the moment these can
+		// be null.
+		sinsp_worker* worker = nullptr;
+		capture_job_handler* capture_handler = nullptr;
+		connection_manager cm(&config,
+		                      &queue,
+		                      worker,
+		                      capture_handler);
+		cm.test_run();
+	});
+
+	// Build the test data
+	std::list<std::string> test_data;
+	for(uint32_t i = 0; i < MAX_QUEUE_LEN; ++i)
+	{
+		test_data.push_back(build_test_string(32));
+	}
+
+	// Push to the queue
+	for(auto msg: test_data)
+	{
+		auto it = std::make_shared<protocol_queue_item>();
+		it->ts_ns = sinsp_utils::get_current_time_ns();
+		it->message_type = draiosproto::message_type::METRICS;
+		it->buffer = msg;
+		queue.put(it, protocol_queue::BQ_PRIORITY_HIGH);
+		msleep(100);
+	}
+
+	// Shut down all the things
+	config.m_terminate = true;
+	fc.stop();
+
+	t.join();
+
+	// Validate the messages received by the fake collector
+	for(auto sent_data: test_data)
+	{
+		ASSERT_TRUE(fc.has_data());
+		auto b = fc.pop_data();
+		std::string received_data(reinterpret_cast<char*>(b.ptr));
+		ASSERT_EQ(sent_data, received_data);
+		delete[] b.ptr;
+	}
+}
---
+++ userspace/dragent/test/fake_collector.cpp
@@ -0,0 +1,210 @@
+/**
+ * @file
+ *
+ * Implements a fake collector for unit testing the connection manager.
+ *
+ * Copyright (c) 2019 Sysdig, Inc. All rights reserved.
+ */
+
+#include "fake_collector.h"
+
+#include <thread>
+#include <iostream>
+#include <cstdint>
+
+// Server / socket stuff
+#include <unistd.h>
+#include <cstdio>
+#include <cstdlib>
+#include <cstring>
+#include <cerrno>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <poll.h>
+
+bool fake_collector::start(uint16_t port)
+{
+	m_run_server = true;
+	struct sockaddr_in addr = {};
+
+	//
+	// Create, configure, and bind the socket
+	//
+	int sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+	if(sock_fd < 0)
+	{
+		m_error_msg = strerror(errno);
+		m_error_code = errno;
+		m_status = server_status::ERRORED;
+		return false;
+	}
+
+	int opt = 1;
+	int ret = setsockopt(sock_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+	if(ret < 0)
+	{
+		m_error_msg = strerror(errno);
+		m_error_code = errno;
+		m_status = server_status::ERRORED;
+		return false;
+	}
+
+	addr.sin_family = AF_INET;
+	addr.sin_addr.s_addr = INADDR_ANY;
+	addr.sin_port = htons(port);
+
+	ret = bind(sock_fd, (struct sockaddr *)&addr, sizeof(addr));
+	if(ret < 0)
+	{
+		m_error_msg = strerror(errno);
+		m_error_code = errno;
+		m_status = server_status::ERRORED;
+		return false;
+	}
+
+	// Find out which port we got
+	socklen_t addr_len = sizeof(addr);
+	ret = getsockname(sock_fd, (struct sockaddr *)&addr, &addr_len);
+	this->m_port = ntohs(addr.sin_port);
+
+	//
+	// Set listening mode on the socket
+	//
+	if(listen(sock_fd, 1) < 0)
+	{
+		m_error_msg = strerror(errno);
+		m_error_code = errno;
+		m_status = server_status::ERRORED;
+		return false;
+	}
+
+	m_status = server_status::RUNNING;
+
+	//
+	// Server loop
+	//
+	std::thread t([sock_fd, &addr, this]()
+	{
+		const uint32_t MAX_SOCKETS = 2;
+		char temp_buf[1024] = {};
+		struct pollfd fds[MAX_SOCKETS] = {};
+		const int timeout = 1000;
+		nfds_t nfds = 1;
+		int agent_fd = -1;
+
+		fds[0].fd = sock_fd;
+		fds[0].events = POLLIN;
+
+
+		while(m_run_server)
+		{
+			//
+			// Server loop
+			//
+			socklen_t addr_len = sizeof(addr);
+			int ret = poll(fds, nfds, timeout);
+
+			if(ret < 0)
+			{
+				m_error_msg = strerror(errno);
+				m_error_code = errno;
+				m_status = server_status::ERRORED;
+				return;
+			}
+
+			if(ret == 0)
+			{
+				continue;
+			}
+
+			nfds_t polled_fds = nfds;
+			// Process all the polled descriptors for valid events
+			for(nfds_t fd = 0; fd < polled_fds; ++fd)
+			{
+				if(fds[fd].revents == 0)
+				{
+					continue;
+				}
+
+				// We have an incoming connection. Accept it, build a new socket, and add it to the list
+				if(fds[fd].fd == sock_fd)
+				{
+					agent_fd = accept(sock_fd, (struct sockaddr *)&addr, &addr_len);
+					if(agent_fd < 0)
+					{
+						m_error_msg = strerror(errno);
+						m_error_code = errno;
+						m_status = server_status::ERRORED;
+						return;
+					}
+					if(nfds > MAX_SOCKETS)
+					{
+						// Too many sockets; can't accept this connection
+						close(agent_fd);
+						continue;
+					}
+					fds[nfds].fd = agent_fd;
+					fds[nfds].events = POLLIN;
+					++nfds;
+				}
+				else // Descriptor is for a client connection that's become readable
+				{
+					int read_ret = read(fds[fd].fd, temp_buf, sizeof(temp_buf));
+					if(read_ret <= 0)
+					{
+						fds[fd].fd = -1;
+						// We'll need to compact the array if we want to support more than 1 client connection
+						--nfds;
+						continue;
+					}
+					temp_buf[read_ret] = '\0';
+
+					// Yay inefficient
+					uint8_t* bufp = new uint8_t[read_ret + 1];
+					memcpy(bufp, temp_buf, read_ret + 1);
+
+					m_received_data.push(buf(bufp, read_ret + 1));
+				}
+			}
+		}
+		m_status = server_status::SHUTDOWN;
+		this->m_port = 0;
+		shutdown(sock_fd, SHUT_RDWR);
+		close(sock_fd);
+	});
+
+	t.detach();
+	return true;
+}
+
+void fake_collector::stop()
+{
+	m_run_server = false;
+}
+
+
+#ifdef FAKE_COLLECTOR_STANDALONE
+
+int main(int argc, char** argv)
+{
+	fake_collector fc;
+
+	fc.start(0);
+
+	std::cout << "Fake collector started at port " << fc.get_port() << std::endl;
+	sleep(60);
+
+	fc.stop();
+
+	std::cout << "Received data:" << std::endl;
+	while(fc.has_data())
+	{
+		auto b = fc.pop_data();
+		printf("\t\"%s\"\n", b.ptr);
+		delete[] b.ptr;
+	}
+
+	return 0;
+}
+
+#endif
