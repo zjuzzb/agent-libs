d54992b2a70df092b6c0d625a10043654a187a48
Krishnan Ramkumar <43651485+krishnan-ramkumar@users.noreply.github.com>
2019-06-11 16:36:17 -0700

    Update agent-slim to minideb:stretch and change to docker-ce-cli [SMAGENT-1725] (#1452)
    * Update agent-slim to minideb:stretch and change to docker-ce-cli
    
    * Clean up some debug info
    
    * Move jackson-databind to 2.9.9

--- docker/generate_agent_docker.py
+++ docker/generate_agent_docker.py
@@ -44,28 +44,24 @@ if image == "agent":
     p['sysdig_repository'] = repo
     p['include_agent_package'] = "apt"
     p['build_kernel_module'] = 1
-    p['backports_debian_release'] = "unstable"
     p['launch_dragent'] = 1
 elif image == "local":
     p['base_docker_image'] = "debian:unstable"
     p['sysdig_repository'] = repo
     p['include_agent_package'] = "local"
     p['build_kernel_module'] = 1
-    p['backports_debian_release'] = "unstable"
     p['launch_dragent'] = 1
 elif image == "agent-slim":
-    p['base_docker_image'] = "bitnami/minideb:jessie"
+    p['base_docker_image'] = "bitnami/minideb:stretch"
     p['sysdig_repository'] = repo
     p['include_agent_package'] = "apt"
     p['build_kernel_module'] = 0
-    p['backports_debian_release'] = "jessie-backports"
     p['launch_dragent'] = 1
 elif image == "agent-kmodule":
     p['base_docker_image'] = "debian:unstable"
     p['sysdig_repository'] = repo
     p['include_agent_package'] = "apt"
     p['build_kernel_module'] = 1
-    p['backports_debian_release'] = "unstable"
     p['launch_dragent'] = 0
 
 if agent_version != "":
