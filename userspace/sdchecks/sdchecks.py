d9abb0291c9b39c0c96aa0f1c7dd9d73fc388a03
akhilcalsoft <akhil.dangore@calsoftinc.com>
2019-06-21 01:05:34 +0530

    Preloaded libnss_files.so.2 file (#1454)

--- userspace/sdchecks/sdchecks.py
+++ userspace/sdchecks/sdchecks.py
@@ -57,7 +57,8 @@ except KeyError:
     SYSDIG_HOST_ROOT = ""
 
 # Loading this now to ensure we have it before potential namespace change
-_LIBNSS = ctypes.CDLL('libnss_dns.so.2')
+_LIBNSS_DNS = ctypes.CDLL('libnss_dns.so.2')
+_LIBNSS_FILES = ctypes.CDLL('libnss_files.so.2')
 _LIBC = ctypes.CDLL('libc.so.6', use_errno=True)
 __NR_setns = 308
 
