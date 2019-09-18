d63c8d914d946b5facc9997d1a7f6c4797823106
Bryan Seay <42394778+bryanseay@users.noreply.github.com>
2019-09-18 10:48:54 -0400

    [SMAGENT-1982] fix benchmark job - sinsp must be destroyed before analyzer (#1628)

--- userspace/libsanalyzer/benchmark/analyzer.bm.cpp
+++ userspace/libsanalyzer/benchmark/analyzer.bm.cpp
@@ -15,18 +15,22 @@ void one_hundred_thousand_reads(benchmark::State& state)
 {
 	for (auto _ : state)
 	{
-		sinsp_mock inspector;
-		inspector.build_event().count(100000).type(PPME_SYSCALL_READ_E).commit();
+		shared_ptr<sinsp_mock> inspector(new sinsp_mock());
+		inspector->build_event().count(100000).type(PPME_SYSCALL_READ_E).commit();
 
 		internal_metrics::sptr_t int_metrics = std::make_shared<internal_metrics>();
 
-		sinsp_analyzer analyzer(&inspector,
+		sinsp_analyzer analyzer(inspector.get(),
 					"/" /*root dir*/,
 					int_metrics,
 					g_sample_handler,
 					g_audit_handler);
 
-		run_sinsp_with_analyzer(inspector, analyzer);
+		run_sinsp_with_analyzer(*inspector, analyzer);
+
+		// For legacy reasons, the inspector must be deleted before the
+		// analyzer.
+		inspector.reset();
 	}
 }
 BENCHMARK(one_hundred_thousand_reads)->Unit(benchmark::kMillisecond);
