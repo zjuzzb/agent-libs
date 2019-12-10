85c9cca853a42b18dfc2f6bdb7575369cefc9cb1
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2019-12-10 10:46:14 +0100

    Add non-templated static builder method for label_limits

--- userspace/libsanalyzer/src/label_limits.cpp
+++ userspace/libsanalyzer/src/label_limits.cpp
@@ -14,6 +14,23 @@ label_limits::label_limits(filter_vec_t filters,
 {
 }
 
+label_limits::sptr_t label_limits::build(
+	filter_vec_t filters,
+	bool log_enabled,
+	uint32_t max_entries,
+	uint64_t expire_seconds)
+{
+	if(log_enabled)
+	{
+		user_configured_limits::enable_logging<label_limits>();
+	}
+	if(!filters.empty() && !label_limits::first_includes_all(filters))
+	{
+		return std::make_shared<label_limits>(std::move(filters), max_entries, expire_seconds);
+	}
+	return nullptr;
+}
+
 INITIALIZE_LOG(label_limits);
 
 
--- userspace/libsanalyzer/src/label_limits.h
+++ userspace/libsanalyzer/src/label_limits.h
@@ -6,9 +6,16 @@
 class label_limits : public user_configured_limits
 {
 public:
+	using sptr_t = std::shared_ptr<label_limits>;
+
 	explicit label_limits(filter_vec_t filters,
 		     uint32_t max_entries = user_configured_limits::MAX_ENTRIES,
 		     uint64_t expire_seconds = user_configured_limits::EXPIRE_SECONDS);
+
+	static sptr_t build(filter_vec_t filters,
+			   bool log_enabled,
+			   uint32_t max_entries = user_configured_limits::MAX_ENTRIES,
+			   uint64_t expire_seconds = user_configured_limits::EXPIRE_SECONDS);
 
 	DEFINE_LOG("Labels");
 	DEFINE_LOG_ENABLED(label_limits);
