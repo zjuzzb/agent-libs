982af30e32b44f95f0604723472a1a367846118a
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2020-04-30 19:00:00 +0200

    Fix gcc version sorting for gcc 10
    See sysdig/probe-builder/build-probe-binaries for the rationale
    behind this change

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -49,7 +49,7 @@ if [ "$SYSDIG_BUILD_KERNEL_MODULE" = "1" ]; then
 	    # gcc-4.8 and gcc-4.9 are symlinks to gcc (for legacy Debian kernels)
 	    # so we skip them while trying to find a candidate to point gcc to
 	    # (symlinking gcc to gcc-4.8 or gcc-4.9 would create a symlink loop)
-	    GCC_BINARY=$(ls -U /usr/bin/gcc-[5-9]* | sort -V | awk -v target=/usr/bin/gcc-${GCC_VERSION} '
+	    GCC_BINARY=$(ls -U /usr/bin/gcc-[5-9]* | sed 's@^/usr/bin/gcc-@@' | sort -V | awk -v target=${GCC_VERSION} '
 			$1 < target { older = $1 }
 			$1 == target { exact = $1 }
 			$1 > target && !newer { newer = $1 }
@@ -60,6 +60,7 @@ if [ "$SYSDIG_BUILD_KERNEL_MODULE" = "1" ]; then
 				else { print older }
 			}
 			')
+	    GCC_BINARY=/usr/bin/gcc-$GCC_BINARY
 	    echo "* Will use $GCC_BINARY to build kernel module if needed"
 	    ln -sf $GCC_BINARY /usr/bin/gcc
 	    break
