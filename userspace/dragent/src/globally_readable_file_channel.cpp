949fad86e3ebd413e591cdc9353b766cc0298a09
Bryan Seay <42394778+bryanseay@users.noreply.github.com>
2020-04-30 09:19:52 -0400

    Change permissions of log folder when the globally readable config is set (#2052)
    [SMAGENT-2403] [SMAGENT-2457]

--- userspace/dragent/src/globally_readable_file_channel.cpp
+++ userspace/dragent/src/globally_readable_file_channel.cpp
@@ -1,6 +1,7 @@
 #include "common_logger.h"
 #include "globally_readable_file_channel.h"
 #include "Poco/DirectoryIterator.h"
+#include "Poco/Path.h"
 #include <sys/stat.h>
 
 COMMON_LOGGER();
@@ -17,10 +18,15 @@ globally_readable_file_channel::globally_readable_file_channel(const std::string
 
 void globally_readable_file_channel::onRotate(const std::string& path)
 {
-	if(m_make_globally_readable)
-	{
-		chmod(path.c_str(), 0644);
-	}
+	if(!m_make_globally_readable) return;
+
+	// Set the permissions of the log file.
+	chmod(path.c_str(), 0644);
+
+	// In testing, the root_dir (default: /opt/draios) had 755 permissions
+	// so the directory that needs to be updated is just the logs directory
+	Poco::Path p(path);
+	chmod(p.parent().toString().c_str(), 0755);
 }
 
 }
