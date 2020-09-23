a10353c8f525c44d1fb5505f3735baf8fb13ad4b
Vadim Zyarko <64800532+VadimZy@users.noreply.github.com>
2020-09-23 10:52:47 -0700

    update cointerface/kube-bench to satisfy aqua  cis-1.5, cis-1.6, gke-1.0, eks-1.0 updates (#2207)

--- userspace/cointerface/src/cointerface/compliance/kube_bench_scraper.go
+++ userspace/cointerface/src/cointerface/compliance/kube_bench_scraper.go
@@ -2,20 +2,20 @@ package compliance
 
 import (
 	"bytes"
-	draiosproto "protorepo/agent-be/proto"
-	"github.com/draios/protorepo/sdc_internal"
 	"encoding/json"
 	"fmt"
+	log "github.com/cihub/seelog"
+	"github.com/draios/protorepo/sdc_internal"
 	"github.com/gogo/protobuf/proto"
 	"io/ioutil"
-	log "github.com/cihub/seelog"
 	"os/exec"
+	draiosproto "protorepo/agent-be/proto"
 	"strings"
 	"text/template"
 	"time"
 )
 
-func findProcess(candidates []string, env []string) (bool) {
+func findProcess(candidates []string, env []string) bool {
 	for _, cand := range candidates {
 		// Check for the candidate at the beginning of the
 		// line, with an optional path
@@ -35,7 +35,31 @@ func findProcess(candidates []string, env []string) (bool) {
 	return false
 }
 
-func (impl *KubeBenchImpl) Variant(stask *ScheduledTask) (string) {
+var (
+	//
+	// Figure out which way to run by looking for an apiserver
+	// process. kube-bench requires additional services such as
+	// the scheduler, etcd, etc to be running on the master, but
+	// at this level we only need to distinguish between master
+	// and node versions.
+	// [vz] now we distinguish targets based on cis version
+	serverCmds = []string{
+		"kube-apiserver",
+		"hyperkube apiserver",
+		"hyperkube kube-apiserver",
+		"apiserver",
+		//OpenShift specific checks
+		"hypershift openshift-kube-apiserver",
+		"openshift start master api",
+		"etcd",
+	}
+	nodeCmds = []string{
+		"hyperkube kubelet",
+		"kubelet",
+	}
+)
+
+func (impl *KubeBenchImpl) Variant(stask *ScheduledTask) string {
 
 	if impl.variant != "" {
 		return impl.variant
@@ -51,93 +75,123 @@ func (impl *KubeBenchImpl) Variant(stask *ScheduledTask) (string) {
 	for _, param := range stask.task.TaskParams {
 		if *param.Key == "variant" {
 			if *param.Val != "master" && *param.Val != "node" {
-				log.Errorf("Ignoring configured variant %s, as it is not \"master\" or \"node\"", *param.Val)
+				_ = log.Errorf("Ignoring configured variant %s, as it is not \"master\" or \"node\"", *param.Val)
 			} else {
 				impl.variant = *param.Val
+				break
 			}
 		}
 	}
 
 	if impl.variant == "none" {
-		//
-		// Figure out which way to run by looking for an apiserver
-		// process. kube-bench requires additional services such as
-		// the scheduler, etcd, etc to be running on the master, but
-		// at this level we only need to distinguish between master
-		// and node versions.
-
-		servercmds := []string{"kube-apiserver",
-			"hyperkube apiserver",
-			"hyperkube kube-apiserver",
-			"apiserver",
-			//OpenShift specific checks
-			"hypershift openshift-kube-apiserver",
-			"openshift start master api"}
-		if findProcess(servercmds, stask.env) {
+		if findProcess(serverCmds, stask.env) {
 			impl.variant = "master"
 		} else {
-			nodecmds := []string{"hyperkube kubelet", "kubelet"}
-			if findProcess(nodecmds, stask.env) {
+			if findProcess(nodeCmds, stask.env) {
 				impl.variant = "node"
 			}
 		}
 	}
 
-	log.Debugf("Variant %s", impl.variant);
+	log.Debugf("Variant %s", impl.variant)
 	return impl.variant
 }
 
-func (impl *KubeBenchImpl) GenArgs(stask *ScheduledTask) ([]string, error) {
-	args := []string{"--json", impl.Variant(stask)}
-
-	// If "benchmark" was provided as a param, add it as a benchmark argument.
+func (impl *KubeBenchImpl) getBenchmark(stask *ScheduledTask) string {
 	for _, param := range stask.task.TaskParams {
 		if *param.Key == "benchmark" {
-			args = append(args, "--benchmark", *param.Val)
+			return *param.Val
 		}
+	}
+	return "none"
+}
+
+func (impl *KubeBenchImpl) genTargets(benchmark, variant string) string {
+
+	switch benchmark {
+	case "cis-1.6":
+		if variant == "node" {
+			return "node,policies"
+		} else {
+			return "master,controlplane,etcd,policies"
+		}
+	case "cis-1.5":
+		if variant == "node" {
+			return "node,policies"
+		} else {
+			return "master,controlplane,etcd,policies"
+		}
+	case "gke-1.0":
+		if variant == "node" {
+			return "node,policies"
+		} else {
+			return "master,controlplane,etcd,policies,managedservices"
+		}
+	case "eks-1.0":
+		if variant == "node" {
+			return "node,policies"
+		} else {
+			return "controlplane,etcd,policies,managedservices"
+		}
+	}
+	return "none"
+}
+
+func (impl *KubeBenchImpl) GenArgs(stask *ScheduledTask) ([]string, error) {
+	args := []string{"--json"} //, impl.Variant(stask)}
+	benchmark := impl.getBenchmark(stask)
+	if benchmark != "none" {
+		args = append(args, "--benchmark", benchmark)
+	}
+
+	targets := impl.genTargets(benchmark, impl.Variant(stask))
+	if targets != "none" {
+		args = append(args, "run", "--targets", targets)
+	} else {
+		args = append(args, impl.Variant(stask))
 	}
 
 	return args, nil
 }
 
 func (impl *KubeBenchImpl) ShouldRun(stask *ScheduledTask) bool {
-	return (impl.Variant(stask) != "none")
+	return impl.Variant(stask) != "none"
 }
 
 type KubeBenchImpl struct {
 	customerId string `json:"customerId"`
-	machineId string `json:"machineId"`
-	variant string `json:"variant"`
+	machineId  string `json:"machineId"`
+	variant    string `json:"variant"`
 }
 
 type kubeTestResult struct {
-	TestNumber string `json:"test_number"`
-	TestDesc string `json:"test_desc"`
-	Type string `json:"type"`
-	TestInfo []string `json:"test_info"`
-	Status string `json:"status"`
+	TestNumber string   `json:"test_number"`
+	TestDesc   string   `json:"test_desc"`
+	Type       string   `json:"type"`
+	TestInfo   []string `json:"test_info"`
+	Status     string   `json:"status"`
 }
 
 type kubeTestSection struct {
-	Section string `json:"section"`
-	Pass uint64 `json:"pass"`
-	Fail uint64 `json:"fail"`
-	Warn uint64 `json:"warn"`
-	Info uint64 `json:"info"`
-	Desc string `json:"desc"`
+	Section string           `json:"section"`
+	Pass    uint64           `json:"pass"`
+	Fail    uint64           `json:"fail"`
+	Warn    uint64           `json:"warn"`
+	Info    uint64           `json:"info"`
+	Desc    string           `json:"desc"`
 	Results []kubeTestResult `json:"results"`
 }
 
 type kubeBenchResults struct {
-	Id string `json:"id"`
-	Version string `json:"version"`
-	Text string `json:"text"`
-	NodeType string `json:"node_type"`
-	Tests []kubeTestSection `json:"tests"`
-	TotalPass uint64 `json:"total_pass"`
-	TotalFail uint64 `json:"total_fail"`
-	TotalWarn uint64 `json:"total_warn"`
-	TotalInfo uint64 `json:"total_info"`
+	Id        string            `json:"id"`
+	Version   string            `json:"version"`
+	Text      string            `json:"text"`
+	NodeType  string            `json:"node_type"`
+	Tests     []kubeTestSection `json:"tests"`
+	TotalPass uint64            `json:"total_pass"`
+	TotalFail uint64            `json:"total_fail"`
+	TotalWarn uint64            `json:"total_warn"`
+	TotalInfo uint64            `json:"total_info"`
 }
 
 // Given a test id, result, and current risk, assign a new risk based
@@ -156,89 +210,93 @@ type kubeBenchResults struct {
 //     - 2.1.4 (Ensure that the --client-ca-file argument is set as appropriate (Scored))
 //     - 2.1.12 (Ensure that the --tls-cert-file and --tls-private-key-file arguments are set as appropriate (Scored))
 //     - Anything in 1.4 or 2.2
+// this risk calculation is obsolete, generally should be performed on BE side given benchmark Levels
+
 func (impl *KubeBenchImpl) AssignRisk(id string, result string, curRisk ResultRisk) ResultRisk {
 	newRisk := low
 
-	highTestIds := map[string]int {
-		"1.1.5": 1,
-		"1.1.6": 1,
-		"1.1.7": 1,
-		"1.1.8": 1,
+	highTestIds := map[string]int{
+		"1.1.5":  1,
+		"1.1.6":  1,
+		"1.1.7":  1,
+		"1.1.8":  1,
 		"1.1.11": 1,
 		"1.1.22": 1,
 		"1.1.29": 1,
 		"1.1.30": 1,
-		"1.3.3": 1,
-		"2.1.2": 1,
-		"2.1.3": 1,
-		"2.1.4": 1,
+		"1.3.3":  1,
+		"2.1.2":  1,
+		"2.1.3":  1,
+		"2.1.4":  1,
 		"2.1.12": 1,
 	}
 
-	if (result != "PASS" &&
+	if result != "PASS" &&
 		((highTestIds[id] == 1 || strings.HasPrefix(id, "1.4")) ||
-		(highTestIds[id] == 2 || strings.HasPrefix(id, "2.2")))) {
+			(highTestIds[id] == 2 || strings.HasPrefix(id, "2.2"))) {
 		newRisk = high
-	} else if (result != "PASS") {
+	} else if result != "PASS" {
 		newRisk = medium
 	}
 
-	if (newRisk == high || (newRisk == medium && curRisk == low)) {
+	if newRisk == high || (newRisk == medium && curRisk == low) {
 		return newRisk
 	}
 
 	return curRisk
 }
 
-func (impl *KubeBenchImpl) Scrape(rootPath string, moduleName string,
+func (impl *KubeBenchImpl) Scrape(
+	rootPath string,
+	moduleName string,
 	task *draiosproto.CompTask,
 	includeDesc bool,
 	evtsChannel chan *sdc_internal.CompTaskEvent,
-	metricsChannel chan string) error {
+	metricsChannel chan string,
+) error {
 
 	evt := &sdc_internal.CompTaskEvent{
-		TaskName: proto.String(moduleName),
+		TaskName:       proto.String(moduleName),
 		InitSuccessful: proto.Bool(true),
 	}
 	cevts := &draiosproto.CompEvents{
-		MachineId: proto.String(impl.machineId),
+		MachineId:  proto.String(impl.machineId),
 		CustomerId: proto.String(impl.customerId),
 	}
 	results := &draiosproto.CompResults{
-		MachineId: proto.String(impl.machineId),
+		MachineId:  proto.String(impl.machineId),
 		CustomerId: proto.String(impl.customerId),
 	}
 
 	metrics := []string{}
-
 	// Read kube-bench's stdout, which contains the test results as json
 	raw, err := ioutil.ReadFile(rootPath + "/stdout.txt")
 	if err != nil {
-		log.Errorf("Could not read json output: %v", err.Error())
+		_ = log.Errorf("Could not read json output: %v", err.Error())
 		return err
 	}
 
-	var bres kubeBenchResults
-	err = json.Unmarshal(raw, &bres)
-
+	// raw is a json array for new aqua kube-bench or a single object for v0.2.4
+	var targets []kubeBenchResults
+	err = json.Unmarshal(raw, &targets)
 	if err != nil {
-		log.Errorf("Could not read json output: %v", err.Error())
-		return err
+		var bRes kubeBenchResults
+		err = json.Unmarshal(raw, &bRes)
+		if err != nil {
+			_ = log.Errorf("Could not read json output: %v", err.Error())
+			return err
+		}
+		targets = append(targets, bRes)
 	}
 
 	timestamp_ns := uint64(time.Now().UnixNano())
 
 	result := &ExtendedTaskResult{
-		Id: *task.Id,
+		Id:          *task.Id,
 		TimestampNS: timestamp_ns,
-		HostMac: impl.machineId,
-		TaskName: *task.Name,
-		ResultSchema: bres.Version,
-		TestsRun: bres.TotalPass + bres.TotalFail + bres.TotalWarn + bres.TotalInfo,
-		PassCount: bres.TotalPass + bres.TotalInfo,
-		FailCount: bres.TotalFail,
-		WarnCount: bres.TotalWarn,
-		Risk: low,
+		HostMac:     impl.machineId,
+		TaskName:    *task.Name,
+		Risk:        low,
 	}
 
 	// If "benchmark" was provided as a param, set the ResultSchema to that value.
@@ -248,104 +306,113 @@ func (impl *KubeBenchImpl) Scrape(rootPath string, moduleName string,
 		}
 	}
 
-	attr := &TaskResultAttribute{
-		K8sNodeType: bres.NodeType,
-	}
+	for _, nodeResult := range targets {
 
-	result.Attributes = append(result.Attributes, *attr)
+		result.ResultSchema = nodeResult.Version
+		result.TestsRun += nodeResult.TotalPass + nodeResult.TotalFail + nodeResult.TotalWarn + nodeResult.TotalInfo
+		result.PassCount += nodeResult.TotalPass + nodeResult.TotalInfo
+		result.FailCount += nodeResult.TotalFail
+		result.WarnCount += nodeResult.TotalWarn
 
-	for _, section := range bres.Tests {
-
-		res_section := &TaskResultSection {
-			SectionId: section.Section,
-			TestsRun: section.Pass + section.Fail + section.Warn + section.Info,
-			PassCount: section.Pass + section.Info,
-			FailCount: section.Fail,
-			WarnCount: section.Warn,
+		attr := &TaskResultAttribute{
+			K8sNodeType: nodeResult.NodeType,
 		}
 
-		if includeDesc {
-			res_section.Description = section.Desc
-		}
+		result.Attributes = append(result.Attributes, *attr)
 
-		metrics_prefix := fmt.Sprintf("compliance.k8s-bench.%v.%v", section.Section, strings.ToLower(strings.Replace(section.Desc, " ", "-", -1)))
-		metrics = append(metrics, fmt.Sprintf("%v.tests_fail:%d|g", metrics_prefix, res_section.FailCount))
-		metrics = append(metrics, fmt.Sprintf("%v.tests_warn:%d|g", metrics_prefix, res_section.WarnCount))
-		metrics = append(metrics, fmt.Sprintf("%v.tests_pass:%d|g", metrics_prefix, res_section.PassCount))
-		metrics = append(metrics, fmt.Sprintf("%v.tests_total:%d|g", metrics_prefix, res_section.TestsRun))
-		metrics = append(metrics, fmt.Sprintf("%v.pass_pct:%f|g", metrics_prefix, (100.0*float64(res_section.PassCount)) / float64(res_section.TestsRun)))
+		for _, section := range nodeResult.Tests {
 
-		for _, test := range section.Results {
-
-			result.Risk = impl.AssignRisk(test.TestNumber, test.Status, result.Risk)
-
-			res_test := &TaskResultTest {
-				TestNumber: test.TestNumber,
+			res_section := &TaskResultSection{
+				SectionId: section.Section,
+				TestsRun:  section.Pass + section.Fail + section.Warn + section.Info,
+				PassCount: section.Pass + section.Info,
+				FailCount: section.Fail,
+				WarnCount: section.Warn,
 			}
 
 			if includeDesc {
-				res_test.Description = test.TestDesc
+				res_section.Description = section.Desc
 			}
 
-			if (test.Status != "PASS" && test.Status != "INFO") {
+			metrics_prefix := fmt.Sprintf("compliance.k8s-bench.%v.%v", section.Section, strings.ToLower(strings.Replace(section.Desc, " ", "-", -1)))
+			metrics = append(metrics, fmt.Sprintf("%v.tests_fail:%d|g", metrics_prefix, res_section.FailCount))
+			metrics = append(metrics, fmt.Sprintf("%v.tests_warn:%d|g", metrics_prefix, res_section.WarnCount))
+			metrics = append(metrics, fmt.Sprintf("%v.tests_pass:%d|g", metrics_prefix, res_section.PassCount))
+			metrics = append(metrics, fmt.Sprintf("%v.tests_total:%d|g", metrics_prefix, res_section.TestsRun))
+			metrics = append(metrics, fmt.Sprintf("%v.pass_pct:%f|g", metrics_prefix, (100.0*float64(res_section.PassCount))/float64(res_section.TestsRun)))
 
-				fields := map[string]string{
-					"Task": moduleName,
-					"SectionDesc": section.Desc,
-					"TestId": test.TestNumber,
-					"TestDesc": test.TestDesc,
-					"TestResult": test.Status,
-					"falco.rule": "compliance_modules",
-				}
-				tmplstr := "Compliance task \"{{.Task}}\" test {{.SectionDesc}}/{{.TestId}} ({{.TestDesc}}) result: {{.TestResult}}."
-				tmpl, err := template.New("test").Parse(tmplstr)
-				if err != nil {
-					log.Errorf("Could not format output string: %v", err.Error())
-					return err
-				}
-				var outputString bytes.Buffer
-				err = tmpl.Execute(&outputString, fields)
-				if err != nil {
-					log.Errorf("Could not format output string: %v", err.Error())
-					return err
+			for _, test := range section.Results {
+
+				result.Risk = impl.AssignRisk(test.TestNumber, test.Status, result.Risk)
+
+				res_test := &TaskResultTest{
+					TestNumber: test.TestNumber,
 				}
 
-				// XXX/mstemm this needs to change pending expanded event stream work.
-				if false {
-					cevt := &draiosproto.CompEvent{
-						TimestampNs: proto.Uint64(timestamp_ns),
-						TaskName: proto.String(*task.Name),
-						Output: proto.String(outputString.String()),
-						OutputFields: fields,
-					};
-
-					cevts.Events = append(cevts.Events, cevt);
+				if includeDesc {
+					res_test.Description = test.TestDesc
 				}
-				if test.Status == "WARN" {
-					res_test.Status = warn
+
+				if test.Status != "PASS" && test.Status != "INFO" {
+
+					// XXX/mstemm this needs to change pending expanded event stream work.
+					if false {
+						fields := map[string]string{
+							"Task":        moduleName,
+							"SectionDesc": section.Desc,
+							"TestId":      test.TestNumber,
+							"TestDesc":    test.TestDesc,
+							"TestResult":  test.Status,
+							"falco.rule":  "compliance_modules",
+						}
+						tmplstr := "Compliance task \"{{.Task}}\" test {{.SectionDesc}}/{{.TestId}} ({{.TestDesc}}) result: {{.TestResult}}."
+						tmpl, err := template.New("test").Parse(tmplstr)
+						if err != nil {
+							log.Errorf("Could not format output string: %v", err.Error())
+							return err
+						}
+						var outputString bytes.Buffer
+						err = tmpl.Execute(&outputString, fields)
+						if err != nil {
+							log.Errorf("Could not format output string: %v", err.Error())
+							return err
+						}
+
+						cevt := &draiosproto.CompEvent{
+							TimestampNs:  proto.Uint64(timestamp_ns),
+							TaskName:     proto.String(*task.Name),
+							Output:       proto.String(outputString.String()),
+							OutputFields: fields,
+						}
+
+						cevts.Events = append(cevts.Events, cevt)
+					}
+					if test.Status == "WARN" {
+						res_test.Status = warn
+					} else {
+						res_test.Status = fail
+					}
 				} else {
-					res_test.Status = fail
+					res_test.Status = pass
 				}
-			} else {
-				res_test.Status = pass
+				res_section.Results = append(res_section.Results, *res_test)
 			}
-			res_section.Results = append(res_section.Results, *res_test)
+			result.Tests = append(result.Tests, *res_section)
 		}
-		result.Tests = append(result.Tests, *res_section)
 	}
-
-	ofbytes, err := json.Marshal(result); if err != nil {
+	ofbytes, err := json.Marshal(result)
+	if err != nil {
 		log.Errorf("Could not serialize test result: %v", err.Error())
 		return err
 	}
 
 	comp_result := &draiosproto.CompResult{
 		TimestampNs: proto.Uint64(result.TimestampNS),
-		TaskName: proto.String(result.TaskName),
-		ModName: task.ModName,
-		TaskId: proto.Uint64(result.Id),
-		Successful: proto.Bool(true),
-		ExtResult: proto.String(string(ofbytes[:])),
+		TaskName:    proto.String(result.TaskName),
+		ModName:     task.ModName,
+		TaskId:      proto.Uint64(result.Id),
+		Successful:  proto.Bool(true),
+		ExtResult:   proto.String(string(ofbytes[:])),
 	}
 
 	results.Results = append(results.Results, comp_result)
@@ -360,7 +427,7 @@ func (impl *KubeBenchImpl) Scrape(rootPath string, moduleName string,
 	metrics = append(metrics, fmt.Sprintf("compliance.k8s-bench.tests_fail:%d|g", result.FailCount))
 	metrics = append(metrics, fmt.Sprintf("compliance.k8s-bench.tests_warn:%d|g", result.WarnCount))
 	metrics = append(metrics, fmt.Sprintf("compliance.k8s-bench.tests_total:%d|g", result.TestsRun))
-	metrics = append(metrics, fmt.Sprintf("compliance.k8s-bench.pass_pct:%f|g", (100.0*float64(result.PassCount)) / float64(result.TestsRun)))
+	metrics = append(metrics, fmt.Sprintf("compliance.k8s-bench.pass_pct:%f|g", (100.0*float64(result.PassCount))/float64(result.TestsRun)))
 
 	for _, metric := range metrics {
 		log.Debugf("Sending kube-bench metric: %v", metric)
---
+++ userspace/cointerface/src/cointerface/compliance/kube_bench_scraper_test.go
@@ -0,0 +1,94 @@
+package compliance
+
+import (
+	"github.com/draios/protorepo/sdc_internal"
+	"os"
+	draiosproto "protorepo/agent-be/proto"
+	"reflect"
+	"testing"
+	"time"
+)
+
+func TestKubeBenchImpl_GenArgs(t *testing.T) {
+	kbImpl := KubeBenchImpl{
+		customerId: "1",
+		machineId:  "2",
+		variant:    "node",
+	}
+	var x uint64 = 1
+	tName := "test"
+	tEnabled := true
+	tParamKey := "benchmark"
+	tParamVal := "cis-1.6"
+	tParam := draiosproto.CompTaskParam{
+		Key: &tParamKey,
+		Val: &tParamVal,
+	}
+	task := draiosproto.CompTask{
+		Id:                   &x,
+		Name:                 &tName,
+		ModName:              &tName,
+		Enabled:              &tEnabled,
+		ScopePredicates:      nil,
+		Schedule:             nil,
+		TaskParams:           []*draiosproto.CompTaskParam{&tParam},
+		XXX_NoUnkeyedLiteral: struct{}{},
+		XXX_unrecognized:     nil,
+		XXX_sizecache:        0,
+	}
+	sTask := ScheduledTask{
+		mgr:          nil,
+		task:         &task,
+		cmd:          nil,
+		cmdStartTime: time.Time{},
+		module:       nil,
+		env:          nil,
+		intervals:    nil,
+	}
+
+	res, err := kbImpl.GenArgs(&sTask)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expect := []string{"--json", "--benchmark", "cis-1.6", "run", "--targets", "node,policies"}
+	if !reflect.DeepEqual(res, expect) {
+		t.Fatalf("expected: %v, got %v", expect, res)
+	}
+
+	kbImpl.variant = "master"
+	res, err = kbImpl.GenArgs(&sTask)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expect = []string{"--json", "--benchmark", "cis-1.6", "run", "--targets", "master,controlplane,etcd,policies"}
+	if !reflect.DeepEqual(res, expect) {
+		t.Fatalf("expected: %v, got %v", expect, res)
+	}
+
+	task.TaskParams = nil
+	res, err = kbImpl.GenArgs(&sTask)
+	if err != nil {
+		t.Fatal(err)
+	}
+	expect = []string{"--json", "master"}
+	if !reflect.DeepEqual(res, expect) {
+		t.Fatalf("expected: %v, got %v", expect, res)
+	}
+
+	_, err = os.Stat("../stdout.txt")
+	if !os.IsNotExist(err) {
+		metrics := make(chan string)
+		events := make(chan *sdc_internal.CompTaskEvent)
+
+		testF := func() {
+			err1 := kbImpl.Scrape("../", "", &task, true, events, metrics)
+			if err1 != nil {
+				t.Fatal(err1)
+			}
+		}
+
+		go testF()
+		event := <-events
+		t.Logf("results: %v", *event.Results.Results[0].ExtResult)
+	}
+}
