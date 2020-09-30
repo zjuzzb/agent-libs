6ae395d74fc98ff12a06128680bc02e14b9b9460
Grzegorz Nosek <grzegorz.nosek@sysdig.com>
2020-09-30 20:09:35 +0200

    Clean up protobuf/grpc generation
    - don't copy unmodified proto files to the build directory
    - DRY all protoc invocations into two cmake functions
    - delete two protobufs we already carry in draios/protorepo
    - fix dependencies for generated code
    
    The last two items were (probably) responsible for spurious build
    failures. preproc.sh copied over secure.proto and profiling.proto
    while (c)make wasn't aware of it and executed protoc in parallel
    with preproc.sh. With enough bad luck, protoc got to read an empty
    file which led to compile errors later on.

--- userspace/cointerface/src/draiosproto/generate.go
+++ userspace/cointerface/src/draiosproto/generate.go
@@ -1,5 +1,5 @@
 // Assumes that cmake will set the env vars
-//go:generate sh -c "protoc -I ${PROTO_SRC_DIR} --gofast_out=plugins=grpc:${PROTO_OUT_DIR} ${PROTO_SRC_DIR}/draios.proto"
-//go:generate sh -c "protoc -I ${PROTO_SRC_DIR} --gofast_out=plugins=grpc:${PROTO_OUT_DIR} ${PROTO_SRC_DIR}/common.proto"
+//go:generate sh -c "protoc -I ${PROTO_BIN_DIR} --gofast_out=plugins=grpc:${PROTO_OUT_DIR} ${PROTO_BIN_DIR}/draios.proto"
+//go:generate sh -c "protoc -I ${PROTO_BIN_DIR} --gofast_out=plugins=grpc:${PROTO_OUT_DIR} ${PROTO_BIN_DIR}/common.proto"
 
 package draiosproto
--- userspace/cointerface/src/promex_pb/generate.go
+++ userspace/cointerface/src/promex_pb/generate.go
@@ -1,4 +1,4 @@
 // Assumes that cmake will set the env vars
-//go:generate sh -c "protoc -I ${PROTO_SRC_DIR} --gofast_out=plugins=grpc,Mdraios.proto=protorepo/agent-be/proto:${PROTO_OUT_DIR} ${PROTO_SRC_DIR}/promex.proto"
+//go:generate sh -c "protoc -I ${PROTO_SRC_DIR} -I ${PROTO_BIN_DIR} --gofast_out=plugins=grpc,Mdraios.proto=protorepo/agent-be/proto:${PROTO_OUT_DIR} ${PROTO_SRC_DIR}/promex.proto"
 
 package promex_pb
