1581a6bdbbb2535e1ac8467e2d0ac67271eb88ae
Nathan Baker <7409217+nathan-b@users.noreply.github.com>
2020-12-14 16:12:46 -0500

    Fix error with arguments to poll() (#2325)

--- userspace/dragent/src/cm_socket.cpp
+++ userspace/dragent/src/cm_socket.cpp
@@ -1077,7 +1077,7 @@ bool cm_poco_socket::has_pending() const
 	{
 		return false;
 	}
-	return m_sockptr->poll(Poco::Timespan(), Poco::Net::Socket::SELECT_READ ||
+	return m_sockptr->poll(Poco::Timespan(), Poco::Net::Socket::SELECT_READ |
 	                                         Poco::Net::Socket::SELECT_ERROR);
 }
 
@@ -1358,7 +1358,7 @@ bool cm_poco_secure_socket::has_pending() const
 	{
 		return false;
 	}
-	return m_sockptr->poll(Poco::Timespan(), Poco::Net::Socket::SELECT_READ ||
+	return m_sockptr->poll(Poco::Timespan(), Poco::Net::Socket::SELECT_READ |
 	                                         Poco::Net::Socket::SELECT_ERROR);
 }
 
