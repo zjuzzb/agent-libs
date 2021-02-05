5d55e3a0a28e7c8a6092e3c4ea72d91b31e20176
Francesco Emmi <francesco.emmi@sysdig.com>
2021-02-05 12:17:27 +0100

    [SMAGENT-2793] log improvements

---
+++ userspace/sdjagent/src/main/java/com/sysdigcloud/sdjagent/exception/CheckContainerException.java
@@ -0,0 +1,8 @@
+package com.sysdigcloud.sdjagent.exception;
+
+public class CheckContainerException extends SdjagentException {
+    public CheckContainerException(String errorMsg) {
+        super(errorMsg);
+    }
+
+}
