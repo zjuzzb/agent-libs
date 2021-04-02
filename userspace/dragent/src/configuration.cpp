3ad9a3cc7059739bdc2a124b448edde8b4530584
Bryan Seay <42394778+bryanseay@users.noreply.github.com>
2021-04-02 18:15:41 -0400

    move curl_get to userspace-shared (#2477)

--- userspace/dragent/src/configuration.cpp
+++ userspace/dragent/src/configuration.cpp
@@ -1,6 +1,7 @@
 #include "common_logger.h"
 #include "configuration.h"
 #include "json_error_log.h"
+#include "network_utils.h"
 #include "running_state.h"
 #include "uri.h"
 #include "windows_helpers.h"
@@ -20,11 +21,11 @@
 
 #include <sys/resource.h>
 #include <sys/time.h>
-#include <curl/curl.h>
 
 using namespace std;
 using namespace Poco;
 using namespace Poco::Net;
+using namespace network_utils;
 
 COMMON_LOGGER("dragent");
 
@@ -1655,65 +1656,6 @@ void dragent_configuration::print_configuration() const
 	for (const auto& item : m_config->errors())
 	{
 		LOG_CRITICAL(item);
-	}
-}
-
-size_t dragent_configuration::curl_write_callback(const char* ptr, size_t size, size_t nmemb, string* json)
-{
-	const std::size_t total = size * nmemb;
-	json->append(ptr, total);
-	return total;
-}
-
-std::string dragent_configuration::curl_get(const std::string& uri, const std::string& buffer)
-{
-	CURL* curl = curl_easy_init();
-	CURLcode res;
-
-	if (curl)
-	{
-		if ((res = curl_easy_setopt(curl, CURLOPT_SSLVERSION, CURL_SSLVERSION_TLSv1_2)) != CURLE_OK)
-		{
-			goto read_error;
-		}
-		if ((res = curl_easy_setopt(curl, CURLOPT_TIMEOUT, 1L)) != CURLE_OK)
-		{
-			goto read_error;
-		}
-		if ((res = curl_easy_setopt(curl, CURLOPT_URL, uri.c_str())) != CURLE_OK)
-		{
-			goto read_error;
-		}
-		if ((res = curl_easy_setopt(curl, CURLOPT_HTTPGET, 1)) != CURLE_OK)
-		{
-			goto read_error;
-		}
-		if ((res = curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1)) != CURLE_OK)
-		{
-			goto read_error;
- 		}
-		if ((res = curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, curl_write_callback)) != CURLE_OK)
-		{
-			goto read_error;
-		}
-		if ((res = curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer)) != CURLE_OK)
-		{
-			goto read_error;
-		}
-		if ((res = curl_easy_perform(curl)) != CURLE_OK)
-		{
-			goto read_error;
-		}
-
-		curl_easy_cleanup(curl);
-		return std::string();
-read_error:
-		curl_easy_cleanup(curl);
-		return std::string(curl_easy_strerror(res));
-	}
-	else
-	{
-		return std::string("Unable to initialize curl");
 	}
 }
 
