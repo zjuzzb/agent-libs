0b2bc1cad92bbc9fab40a9bdad28473e9f4d41be
Angelo Puglisi <angelo.puglisi@sysdig.com>
2021-06-24 16:35:12 +0200

    SMAGENT-3050 Agent Should Print Number of CPU Cores and Memory Amount at Startup

--- userspace/dragent/src/dragent.cpp
+++ userspace/dragent/src/dragent.cpp
@@ -1127,6 +1127,10 @@ void dragent_app::log_sysinfo()
 	if (error == 0)
 	{
 		LOG_INFO("System uptime: " + NumberFormatter::format(info.uptime) + "s");
+		LOG_INFO(
+		    "Total physical memory: " +
+		    Poco::NumberFormatter::format(info.totalram * info.mem_unit / 1024.0 / 1024 / 1024, 2) +
+		    " GiB");
 	}
 	else
 	{
@@ -1141,6 +1145,8 @@ void dragent_app::log_sysinfo()
 	{
 		LOG_WARNING("Cannot get kernel version");
 	}
+
+	LOG_INFO("Number of processors: " + NumberFormatter::format(sysconf(_SC_NPROCESSORS_ONLN)));
 }
 
 
