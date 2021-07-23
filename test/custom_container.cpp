4ea890f10f70207d32b6b536a67e51c3023c906d
Joseph Pittman <joseph.pittman@sysdig.com>
2021-07-23 18:40:09 -0400

    [SSPROD-7548] Incorporated review comments
    - Use const qualifier for catch parameter
    - Use stream !fail() instead of stream good(), to preserve functional equivalence
      with stream bool() operator

--- test/custom_container.cpp
+++ test/custom_container.cpp
@@ -36,7 +36,7 @@ TEST_F(custom_container_f, match)
 	bool thrown = false;
 	try {
 		m.render(out, 3);
-	} catch (Poco::RuntimeException &e) {
+	} catch (const Poco::RuntimeException &e) {
 		thrown = true;
 	}
 	ASSERT_TRUE(thrown);
