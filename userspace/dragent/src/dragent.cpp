08515fdc728f9fba743f1aacd2d0e6e45197db16
Shan Gurunathan <81978019+shanguru@users.noreply.github.com>
2021-10-27 08:20:08 -0400

    Revert the fix for 3140 (#2787)

--- userspace/dragent/src/dragent.cpp
+++ userspace/dragent/src/dragent.cpp
@@ -889,9 +889,8 @@ int dragent_app::main(const std::vector<std::string>& args)
 	}
 
 #ifndef CYGWING_AGENT
-	if (m_configuration.python_present() &&
-	    (feature_manager::instance().get_enabled(APP_CHECKS) ||
-	     (m_configuration.m_prom_conf.enabled() && !prom_helper::c_use_promscrape.get_value())))
+	if (m_configuration.python_present() && (feature_manager::instance().get_enabled(APP_CHECKS) ||
+	                                         m_configuration.m_prom_conf.enabled()))
 	{
 		m_sdchecks_pipes = make_unique<errpipe_manager>();
 		auto state = &m_subprocesses_state["sdchecks"];
@@ -1505,7 +1504,7 @@ int dragent_app::sdagent_main()
 		std::shared_ptr<app_checks_proxy> the_app_checks_proxy = nullptr;
 		if ((feature_manager::instance().get_enabled(APP_CHECKS) &&
 		     !m_configuration.m_app_checks.empty()) ||
-		    (m_configuration.m_prom_conf.enabled() && !prom_helper::c_use_promscrape.get_value()))
+		    m_configuration.m_prom_conf.enabled())
 		{
 			bool app_check_thread = c_app_check_thread.get_value();
 			the_app_checks_proxy =
