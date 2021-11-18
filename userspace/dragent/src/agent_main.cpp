6f5b8a1d211c1393991b177d0161d1ae66b623b0
Angelo Puglisi <angelo.puglisi@sysdig.com>
2021-11-18 18:56:47 +0100

    Catch and print early exceptions

--- userspace/dragent/src/agent_main.cpp
+++ userspace/dragent/src/agent_main.cpp
@@ -2,6 +2,19 @@
 
 int main(int argc, char** argv)
 {
-	dragent_app app;
-	return app.run(argc, argv);
+	try
+	{
+		dragent_app app;
+		return app.run(argc, argv);
+	}
+	catch(const Poco::Exception& e)
+	{
+		std::cerr <<  e.displayText() << std::endl;
+		throw;
+	}
+	catch(const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		throw;
+	}
 }
